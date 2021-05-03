c46ac27de507f1e064bb44afa5862499b0c081b9
NIcolas Epstein <nilueps@gmail.com>
2021-05-03 10:13:30 -0400

    cleaner sccs import and button component converted to FonctionComponent and using classnames util

--- libs/shared/ui-toolkit/src/lib/components/button/button.component.tsx
+++ libs/shared/ui-toolkit/src/lib/components/button/button.component.tsx
@@ -1,48 +1,68 @@
-import React, {
-  ButtonHTMLAttributes,
-} from 'react';
+import React, { ButtonHTMLAttributes, FunctionComponent } from 'react';
 import classNames from 'classnames';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-  kind?: 'primary' | 'secondary' | 'danger' | 'success' | 'warning' | 'info' | 'light' | 'dark' | 'link'
-  size?: 'sm' | 'lg'
-  outline?: boolean
-}
+  kind?:
+    | 'primary'
+    | 'secondary'
+    | 'danger'
+    | 'success'
+    | 'warning'
+    | 'info'
+    | 'light'
+    | 'dark'
+    | 'link';
+  size?: 'sm' | 'lg';
+  outline?: boolean;
+};
 
-export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  (props: ButtonProps, ref) => {
-    const {kind, size, outline, className, children, ...otherProps} = props;
-    const classes: string[] = [];
-    classes.push('btn');
-    if (kind) {
-      classes.push('btn-' + (outline ? 'outline-' : '') + kind);
-    }
-    if (size) {
-      classes.push('btn-' + size);
-    }
-    return (<button ref={ref} {...otherProps} className={classNames(className, ...classes)}>{children}</button>);
-  });
+export const Button: FunctionComponent<ButtonProps> = (
+  props: ButtonProps,
+  ref
+) => {
+  const { kind, size, outline, className, children, ...otherProps } = props;
+  const classes: string[] = [];
+  classes.push('btn');
+  if (kind) {
+    classes.push('btn-' + (outline ? 'outline-' : '') + kind);
+  }
+  if (size) {
+    classes.push('btn-' + size);
+  }
+  const btnClass = classNames(className, classes);
+  return (
+    <button ref={ref} {...otherProps} className={btnClass}>
+      {children}
+    </button>
+  );
+};
 
 type CloseButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-  size?: 'sm' | 'lg'
-}
+  size?: 'sm' | 'lg';
+};
 
 export const CloseButton = (() => {
-  const cmp = React.forwardRef<HTMLButtonElement, CloseButtonProps>(
-    ({size, className, children, ...props}: ButtonProps, ref) => {
-      className = classNames(className, 'btn-close');
-      if (size) {
-        className = classNames(className, 'btn-' + size);
-      }
-      return <button
-        ref={ref}
-        className={className}
-        {...props}>{children}
-      </button>;
+  const cmp: FunctionComponent<CloseButtonProps> = (
+    { size, className, children, ...props }: ButtonProps,
+    ref
+  ) => {
+    className = classNames(className, {
+      btn: true,
+      'btn-close': true,
+      'btn-size': size,
     });
+    // if (size) {
+    //   className = classNames(className, 'btn-' + size);
+    // }
+    return (
+      <button ref={ref} className={className} {...props}>
+        {children}
+      </button>
+    );
+  };
   cmp.defaultProps = {
     type: 'button',
-    'aria-label': 'Close'
+    'aria-label': 'Close',
   };
   cmp.displayName = 'CloseButton';
   return cmp;
--- libs/shared/ui-toolkit/src/lib/components/demo/demo.stories.tsx
+++
@@ -1,3 +0,0 @@
-import { storiesOf } from "@storybook/react";
-
-storiesOf("demo component", module).add("story name", () => (<p>whatever</p>))
