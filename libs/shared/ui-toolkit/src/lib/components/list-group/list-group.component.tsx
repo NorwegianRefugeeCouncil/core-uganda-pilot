7489bdf1ff408347bfaed8b3534d53857470cd86
NIcolas Epstein <nilueps@gmail.com>
2021-05-03 11:11:34 -0400

    convert objects to FunctionComponent, replace custom addClasses with classnames

--- libs/shared/ui-toolkit/src/lib/components/list-group/list-group.component.tsx
+++ libs/shared/ui-toolkit/src/lib/components/list-group/list-group.component.tsx
@@ -1,54 +1,76 @@
 import { AnchorHTMLAttributes, FunctionComponent, HTMLAttributes } from 'react';
-import { addClasses } from '../../utils/utils';
-
-export interface ListGroupProps extends HTMLAttributes<HTMLDivElement | HTMLUListElement> {
-  flush?: boolean
-  isActionListGroup?: boolean
-  numbered?: boolean
+import classNames from 'classnames';
+export interface ListGroupProps
+  extends HTMLAttributes<HTMLDivElement | HTMLUListElement> {
+  flush?: boolean;
+  isActionListGroup?: boolean;
+  numbered?: boolean;
 }
 
-const ListGroup: FunctionComponent<ListGroupProps> = ({flush, isActionListGroup, numbered, ...props}) => {
-
-  const classes = ['list-group'];
-  if (flush) {
-    classes.push('list-group-flush');
-  }
+const ListGroup: FunctionComponent<ListGroupProps> = ({
+  flush,
+  isActionListGroup,
+  numbered,
+  ...props
+}) => {
+  const classes = (obj) => classNames(props.className, 'list-group', obj);
   if (isActionListGroup) {
-    return (<div {...props} className={addClasses(props.className, ...classes)}>{props.children}</div>);
+    return (
+      <div {...props} className={props.className}>
+        {props.children}
+      </div>
+    );
   } else if (numbered) {
-    classes.push('list-group-numbered');
-    return (<ol  {...props} className={addClasses(props.className, ...classes)}>{props.children}</ol>);
+    return (
+      <ol
+        {...props}
+        className={classes({
+          'list-group-flush': flush,
+          'list-group-numbered': true,
+        })}
+      >
+        {props.children}
+      </ol>
+    );
   } else {
-    return (<ul  {...props} className={addClasses(props.className, ...classes)}>{props.children}</ul>);
+    return (
+      <ul {...props} className={classes({ 'list-group-flush': flush })}>
+        {props.children}
+      </ul>
+    );
   }
 };
 
 export interface ListGroupItemProps {
-  active?: boolean
-  disabled?: boolean
-  isAction?: boolean
+  active?: boolean;
+  disabled?: boolean;
+  isAction?: boolean;
 }
 
-const ListGroupItem: FunctionComponent<(HTMLAttributes<HTMLLIElement> | AnchorHTMLAttributes<HTMLAnchorElement>) & ListGroupItemProps> = ({active, isAction, disabled, ...props}) => {
-  const classes = ['list-group-item'];
-  if (active) {
-    classes.push('active');
-  }
-  if (disabled) {
-    classes.push('disabled');
-  }
-  if (isAction) {
-    classes.push('list-group-item-action');
-  }
+const ListGroupItem: FunctionComponent<
+  (HTMLAttributes<HTMLLIElement> | AnchorHTMLAttributes<HTMLAnchorElement>) &
+    ListGroupItemProps
+> = ({ active, isAction, disabled, ...props }) => {
+  const classes = classNames('list-group-item', {
+    active,
+    disabled,
+    'list-group-item-action': isAction,
+  });
   if (isAction) {
     const p = props as AnchorHTMLAttributes<HTMLAnchorElement>;
-    return (<a {...p} className={addClasses(props.className, ...classes)}>{props.children}</a>);
+    return (
+      <a {...p} className={classNames(props.className, classes)}>
+        {props.children}
+      </a>
+    );
   } else {
     const p = props as HTMLAttributes<HTMLLIElement>;
-    return (<li {...p} className={addClasses(props.className, ...classes)}>{props.children}</li>);
+    return (
+      <li {...p} className={classNames(props.className, classes)}>
+        {props.children}
+      </li>
+    );
   }
-
 };
-
 
 export { ListGroup, ListGroupItem };
