ab0bb3f54639911ba70b0507244e9b4f9e94ae7a
Ludovic Cleroux <ludovic.cleroux@gmail.com>
2021-05-06 16:35:34 +0200

    cleanup api. add store watch function

---
+++ apps/api/pkg/endpoints/handlers/formdefinitions/handler.go
@@ -0,0 +1,119 @@
+package formdefinitions
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/nrc-no/core/apps/api/pkg/apis"
+	"github.com/nrc-no/core/apps/api/pkg/storage"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+const (
+	resourceName = "formdefinitions"
+	groupName    = "core.nrc.no"
+	apiVersion   = "core.nrc.no/v1"
+)
+
+type Handler struct {
+	storage storage.Interface
+}
+
+func (h *Handler) Get(w http.ResponseWriter, req *http.Request) {
+
+	id, ok := mux.Vars(req)["id"]
+	if !ok {
+		http.Error(w, "unable to get resource id", http.StatusBadRequest)
+		return
+	}
+
+	ctx := req.Context()
+
+	var formDefinition apis.FormDefinition
+	if err := h.storage.Get(ctx, fmt.Sprintf("%s-%s-%s", groupName, resourceName, id), &formDefinition); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	bodyBytes, err := json.Marshal(formDefinition)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(bodyBytes)
+
+}
+
+func (h *Handler) Update(w http.ResponseWriter, req *http.Request) {
+
+	ctx := req.Context()
+	bodyBytes, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var formDefinition apis.FormDefinition
+	if err := json.Unmarshal(bodyBytes, &formDefinition); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var out apis.FormDefinition
+	if err := h.storage.Update(ctx, "core.nrc.no/formdefinitions/"+formDefinition.ObjectMeta.UID, &formDefinition, &out); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	responseBytes, err := json.Marshal(out)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(responseBytes)
+
+}
+
+func (h *Handler) Post(w http.ResponseWriter, req *http.Request) {
+
+	ctx := req.Context()
+
+	bodyBytes, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var formDefinition apis.FormDefinition
+	if err := json.Unmarshal(bodyBytes, &formDefinition); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	formDefinition.ObjectMeta.UID = ""
+	formDefinition.ObjectMeta.CreationTimestamp = time.Now().UTC()
+	formDefinition.ObjectMeta.DeletionTimestamp = nil
+	formDefinition.SetAPIVersion(apiVersion)
+
+	var out apis.FormDefinition
+	if err := h.storage.Create(ctx, "formdefinitions-"+formDefinition.ObjectMeta.UID, &formDefinition, &out); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	responseBytes, err := json.Marshal(&out)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(responseBytes)
+
+}
