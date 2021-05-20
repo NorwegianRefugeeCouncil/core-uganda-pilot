3689ad12047b90621a42cf31398323614eed89fd
Nicolas Epstein <nilueps@gmail.com>
2021-05-20 10:24:51 -0400

    make custom nx executor to copy bootstrap.css

---
+++ ui/tools/executors/copy-bootstrap-css/impl.ts
@@ -0,0 +1,19 @@
+import * as childProcess from 'child_process';
+
+interface Options {
+  targetPath: string;
+}
+
+export default async function (
+  _options: Options
+): Promise<{ success: boolean }> {
+  const child = childProcess.spawn('cp', [
+    'dist/libs/shared/bootstrap/assets/css/bootstrap.css',
+    _options.targetPath,
+  ]);
+  return new Promise<{ success: boolean }>((res) => {
+    child.on('close', (code) => {
+      res({ success: code === 0 });
+    });
+  });
+}
