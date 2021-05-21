85d18c4a14ce4fed31dc4ba4757855818bbe1f6e
Nicolas Epstein <nilueps@gmail.com>
2021-05-21 16:23:33 -0400

    clean bootstrap library and build process

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -2,11 +2,11 @@ module.exports = {
   projects: [
     '<rootDir>/apps/core-web-app',
     '<rootDir>/libs/shared/ui-toolkit',
-    '<rootDir>/libs/shared/bootstrap',
     '<rootDir>/libs/formbuilder',
     '<rootDir>/libs/formrenderer',
     '<rootDir>/apps/showcase',
     '<rootDir>/libs/core-ui',
     '<rootDir>/libs/shared/api-client',
+    '<rootDir>/libs/shared/bootstrapp',
   ],
 };
