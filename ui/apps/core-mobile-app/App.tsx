2dd8f3a984ba6ab5b7b2e10c801feac713338a5d
Nicolas Epstein <nilueps@gmail.com>
2021-05-26 08:29:57 -0400

    module resolution error

--- ui/apps/core-mobile-app/App.tsx
+++ ui/apps/core-mobile-app/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TopBar } from '@core/core-ui';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TopBar />
     </View>
   );
 }
@@ -14,6 +15,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
