3a0675bc820036c8ae678f09cf9618a3ea6e99bb
Ludovic Cleroux <ludovic.cleroux@gmail.com>
2021-05-26 14:31:45 +0200

    discovery client export

--- ui/libs/shared/api-client/src/index.ts
+++ ui/libs/shared/api-client/src/index.ts
@@ -5,3 +5,6 @@ export * from './lib/rest';
 export * from './lib/client';
 export * from './lib/client/core';
 export * from './lib/client/core/v1';
+export * from './lib/client/discovery';
+export * from './lib/client/discovery/v1';
+
