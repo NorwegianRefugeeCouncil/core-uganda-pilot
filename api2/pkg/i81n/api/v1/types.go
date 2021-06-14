901e178ac63cbf293e063cb9984569c5bbdce98d
Ludovic Cleroux <ludovic.cleroux@gmail.com>
2021-06-14 07:31:23 +0200

    add bson tags

--- api2/pkg/i81n/api/v1/types.go
+++ api2/pkg/i81n/api/v1/types.go
@@ -3,8 +3,8 @@ package v1
 type Locale string
 
 type Translation struct {
-	Locale      Locale `json:"locale" bson:"locale" bson:"locale"`
-	Translation string `json:"translation" bson:"translation" bson:"translation"`
+	Locale      Locale `json:"locale" bson:"locale"`
+	Translation string `json:"translation" bson:"translation"`
 }
 
 type Translations []Translation
