41fdec39dc1ce1ef3c435c4eca31f053c5c82daf
Nicolas Epstein <nilueps@gmail.com>
2021-06-23 17:00:31 -0400

    user can log in, but cypress complains about cross-origin request

--- e2e/cypress/integration/spec.js
+++ e2e/cypress/integration/spec.js
@@ -1,12 +1,37 @@
-describe("The home page", () => {
-    // TODO reset and seed the database before running tests
-    it("successfully loads", () => {
-        cy.visit("/")
+let accessToken
+describe("Obtain access token", () => {
+    it("gets an access token", () => {
+        cy.request({
+            method: "POST",
+            url: "http://localhost:8080/auth/realms/nrc/protocol/openid-connect/token",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: "client_id=api&client_secret=e6486272-039d-430f-b3c7-47887aa9e206&grant_type=password&username=admin&password=admin&scope=openid",
+        }).then(response => {
+            accessToken = response.body.access_token
+            console.log(accessToken)
+        })
     })
-    it("redirects to auth", () => {
-        cy.url().should('include', "auth/realms/nrc/protocol/openid-connect/auth")
-    })
-    it("lets a user log in", () => {
+// cy.server({
+//     onAnyRequest: (route, proxy) => {
+//         proxy.xhr.setRequestHeader("Authorization", "Bearer " + response.body.access_token)
+//     }
+// })
+})
+// TODO reset and seed the database before running tests
+
+beforeEach(() => {
+    cy.intercept(
+        {url: "*"},
+        req => req.headers["Authorization"] = `Bearer ${accessToken}`
+    )
+})
+
+
+describe("User log in", () => {
+    it('is accessible', () => {
+        cy.visit('/')
         cy.get("input[name=username]").type("admin")
         cy.get("input[name=password]").type("admin{enter}")
     })
