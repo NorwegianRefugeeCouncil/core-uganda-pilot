d74dff1fcbe405da6b8515d99f2f1683fc9fe710
Nicolas Epstein <nilueps@gmail.com>
2021-06-23 11:09:45 -0400

    install cypress-keycloak; downgrade cypress to ^6.5.0

--- e2e/cypress/integration/spec.js
+++ e2e/cypress/integration/spec.js
@@ -1,5 +1,13 @@
 describe("The home page", () => {
+    // TODO reset and seed the database before running tests
     it("successfully loads", () => {
         cy.visit("/")
+    })
+    it("redirects to auth", () => {
+        cy.url().should('include', "auth/realms/nrc/protocol/openid-connect/auth")
+    })
+    it("lets a user log in", () => {
+        cy.get("input[name=username]").type("admin")
+        cy.get("input[name=password]").type("admin{enter}")
     })
 })
