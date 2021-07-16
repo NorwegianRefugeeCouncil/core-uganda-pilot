7aad7be25e19f6ead460de25a103f4f0e9feaf53
Ludovic Cleroux <ludovic.cleroux@gmail.com>
2021-07-16 09:46:14 +0200

    COR-153 fix oidc issuer check

--- api/pkg/apps/webapp/auth.go
+++ api/pkg/apps/webapp/auth.go
@@ -14,16 +14,21 @@ func (s *Server) WithAuth() func(handler http.Handler) http.Handler {
 	return func(handler http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 
+			// When in development mode, the user may provide a custom header
+			// containing the email of the user s.he wants to impersonate,
+			// effectively bypassing authorization
 			if s.environment == "Development" {
 				authUserEmail := req.Header.Get("X-E2E-Authenticated-User-Email")
 				if len(authUserEmail) != 0 {
-					s.setAuthenticatedUserSubject(w, req, authUserEmail, handler)
+					s.dangerouslySetAuthenticatedUserUsingEmail(w, req, authUserEmail, handler)
 					return
 				}
 			}
 
 			// Do no perform auth when user tries to access login or callback
 			// Because that is performing auth itself
+			// If we would return a unauthorized response, then the user could
+			// never log in, as logging in would itself be "unauthorized"
 			if req.URL.Path == "/login" || req.URL.Path == "/callback" {
 				handler.ServeHTTP(w, req)
 				return
@@ -32,6 +37,8 @@ func (s *Server) WithAuth() func(handler http.Handler) http.Handler {
 			// Get the access token from the session
 			token := s.sessionManager.GetString(req.Context(), "access-token")
 
+			// Retrieve token from Authorization header if the token
+			// was not present in the session
 			if len(token) == 0 {
 				authHeader := req.Header.Get("Authorization")
 				authHeaderParts := strings.Split(authHeader, " ")
@@ -57,9 +64,9 @@ func (s *Server) WithAuth() func(handler http.Handler) http.Handler {
 				return
 			}
 
+			// If the user is not active, then redirect to login
 			if !*res.Payload.Active {
 				http.Redirect(w, req, "/login", http.StatusTemporaryRedirect)
-				// http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
 
@@ -69,7 +76,7 @@ func (s *Server) WithAuth() func(handler http.Handler) http.Handler {
 
 }
 
-func (s *Server) setAuthenticatedUserSubject(w http.ResponseWriter, req *http.Request, authUserEmail string, handler http.Handler) {
+func (s *Server) dangerouslySetAuthenticatedUserUsingEmail(w http.ResponseWriter, req *http.Request, authUserEmail string, handler http.Handler) {
 	ctx := req.Context()
 
 	authUsers, err := s.iamAdminClient.Parties().Search(ctx, iam.PartySearchOptions{
