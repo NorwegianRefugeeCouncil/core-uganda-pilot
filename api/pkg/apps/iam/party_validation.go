ae91285fa0af7fed051f2480f416a05985d9b66d
Nicolas Epstein <nilueps@gmail.com>
2021-07-19 11:15:57 -0400

    remove comment and create associated jira issues

--- api/pkg/apps/iam/party_validation.go
+++ api/pkg/apps/iam/party_validation.go
@@ -12,13 +12,7 @@ func ValidateParty(party *Party, path *validation.Path) validation.ErrorList {
 	}
 
 	// Validate Attributes
-	//	// For now Party Attributes are just a map[string][]string but eventually
-	//	// we will want TODO some changes to have more information about value types
-	//	// and validation
-	//attributesPath := path.Child("attributes")
-	//for name, values := range party.Attributes {
-	//	attributePath := attributesPath.Key(name)
-	//}
+	// TODO COR-159
 
 	return errs
 }
