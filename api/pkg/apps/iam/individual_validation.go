5dca34b9d6fff8258d111cc9affe3a9679b128a4
Nicolas Epstein <nilueps@gmail.com>
2021-07-19 11:24:09 -0400

    add validation for individual having the individual party type

--- api/pkg/apps/iam/individual_validation.go
+++ api/pkg/apps/iam/individual_validation.go
@@ -1,10 +1,16 @@
 package iam
 
-import "github.com/nrc-no/core/pkg/validation"
+import (
+	"fmt"
+	"github.com/nrc-no/core/pkg/validation"
+)
 
 func ValidateIndividual(individual *Individual, path *validation.Path) validation.ErrorList {
 	// Validate Party
 	errs := ValidateParty(individual.Party, path)
+	if !individual.Party.HasPartyType(IndividualPartyType.ID) {
+		errs = append(errs, validation.InternalError(path.Child("partyTypeIds"), fmt.Errorf("individual is missing the individual party type")))
+	}
 
 	return errs
 }
