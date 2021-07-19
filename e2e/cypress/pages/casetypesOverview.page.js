48a5e627fa3bfeccdc9e548a2b98371999e49657
senyaoh <31976816+senyaoh@users.noreply.github.com>
2021-07-19 16:51:30 +0200

    COR-92: Rewrite e2e test with page object pattern (#96)
    * Added e2e test for new attribute, casetype, relationshiptype with page object, update base url to https, adde dotenv to node package
    
    * Revert e2e/package-lock.json changes
    
    * Added test for entitytype, fix eslint issues
    
    * Added individual tests, remove old test files
    
    * Added casetype template, remove settings/countries path
    
    * change entitytype back to partytype
    
    * confirm castype added and update casetype test
    
    * Added new Casetype test, partytype test
    
    * Update test for individual, relationshiptype, attribute
    
    * Added test for searching and adding new member to a team
    
    * Added test for searching and adding new member to a team
    
    * make compatible with latest
    
    * change data-cy to data-testid
    
    * update cypress to latest; remove unused dotenv
    
    * Updated casetype and team tests
    
    * Fix case tests
    
    Co-authored-by: Nicolas Epstein <nilueps@gmail.com>

---
+++ e2e/cypress/pages/casetypesOverview.page.js
@@ -0,0 +1,21 @@
+import { Urls } from '../helpers';
+import NewCaseTypePage from './newCasetype.page';
+
+const CASETYPE_ROWS = '[data-testid=casetype]';
+
+export default class CasetypesOverviewPage {
+    visitPage = () => {
+        cy.log('navigating to %s', Urls.CASETYPE_URL);
+        cy.visit(Urls.CASETYPE_URL);
+        return this;
+    };
+
+    selectCasetype = () => {
+        return cy.get(CASETYPE_ROWS).last();
+    };
+
+    visitCasetype = () => {
+        cy.get(CASETYPE_ROWS).last().click();
+        return new NewCaseTypePage();
+    };
+}
