8b83d4045d53cdad00f69f3888c4d0c4565a382a
Robert Focke <robert_focke@yahoo.com>
2021-07-28 12:53:35 +0200

    add handling for existing credentials, and a check if there is more than one credential present

--- api/pkg/apps/login/credentials_post.go
+++ api/pkg/apps/login/credentials_post.go
@@ -1,14 +1,37 @@
 package login
 
 import (
+	"errors"
+	"go.mongodb.org/mongo-driver/bson"
 	"net/http"
 )
 
 func (s *Server) PostCredentials(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+
 	var payload SetCredentialPayload
 	if err := s.Bind(req, &payload); err != nil {
 		s.Error(w, err)
 		return
+	}
+
+	res, err := s.Collection.Find(ctx, bson.M{"partyId": payload.PartyID})
+	if err != nil {
+		s.Error(w, err)
+		return
+	}
+
+	var credentials []*Credential
+	for {
+		if !res.Next(ctx) {
+			break
+		}
+		var c Credential
+		if err := res.Decode(&c); err != nil {
+			s.Error(w, err)
+			return
+		}
+		credentials = append(credentials, &c)
 	}
 
 	saltedHash, err := HashAndSalt(s.BCryptCost, []byte(payload.PlaintextPassword))
@@ -17,16 +40,39 @@ func (s *Server) PostCredentials(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	var newCredential = Credential{
-		PartyID: payload.PartyID,
-		Hash:    saltedHash,
+	var returnValue Credential
+
+	// no credentials found, need to create new
+	if len(credentials) == 0 {
+		var newCredential = Credential{
+			PartyID: payload.PartyID,
+			Hash:    saltedHash,
+		}
+
+		_, err = s.Collection.InsertOne(ctx, newCredential)
+		if err != nil {
+			s.Error(w, err)
+			return
+		}
+
+		returnValue = newCredential
 	}
 
-	_, err = s.Collection.InsertOne(req.Context(), newCredential)
-	if err != nil {
-		s.Error(w, err)
+	// existing credential found, updating
+	if len(credentials) == 1 {
+		err = s.SetPassword(ctx, payload.PartyID, payload.PlaintextPassword)
+
+		if err != nil {
+			s.Error(w, err)
+			return
+		}
+	}
+
+	// more than one credential found, error
+	if len(credentials) > 1 {
+		s.Error(w, errors.New("more than one credential found for user"))
 		return
 	}
 
-	s.json(w, http.StatusOK, newCredential)
+	s.json(w, http.StatusOK, returnValue)
 }
