f442c7a182d41f1e2fff4589ff8f00cb2a1759fd
Robert Focke <robert_focke@yahoo.com>
2021-07-30 17:10:58 +0200

    add list filtering tests

--- api/pkg/apps/attachments/attachment_test.go
+++ api/pkg/apps/attachments/attachment_test.go
@@ -7,6 +7,8 @@ import (
 
 func (s *Suite) TestAttachments() {
 	s.Run("API", func() { s.testAttachmentAPI() })
+	s.SetupTest()
+	s.Run("List filtering", func() { s.testAttachmentListFilter() })
 }
 
 func (s *Suite) testAttachmentAPI() {
@@ -51,4 +53,45 @@ func (s *Suite) testAttachmentAPI() {
 		s.T().FailNow()
 	}
 	assert.Contains(s.T(), list.Items, get)
+}
+
+func (s *Suite) testAttachmentListFilter() {
+	const nOwners = 10
+	const nAttachmentsPerOwner = 10
+
+	var owners []string
+	// create owners
+	for i := 0; i < nOwners; i++ {
+		owners = append(owners, s.NewUUID())
+	}
+
+	for _, owner := range owners {
+		attachmentsForOwner := []*attachments.Attachment{}
+
+		// create attachments for each owner
+		for i := 0; i < nAttachmentsPerOwner; i++ {
+			attachment := s.aMockAttachment(owner)
+			created, err := s.client.Create(s.Ctx, attachment)
+			if !assert.NoError(s.T(), err) {
+				s.T().FailNow()
+			}
+			attachment.ID = created.ID
+			assert.Equal(s.T(), attachment, created)
+			attachmentsForOwner = append(attachmentsForOwner, attachment)
+		}
+
+		// test filter for each owner returns expected amount of records
+		list, err := s.client.List(s.Ctx, attachments.AttachmentListOptions{
+			AttachedToID: owner,
+		})
+		if !assert.NoError(s.T(), err) {
+			s.T().FailNow()
+		}
+		assert.Equal(s.T(), len(list.Items), nAttachmentsPerOwner)
+
+		// test all expected attachments are in returned list
+		for _, attachment := range attachmentsForOwner {
+			assert.Contains(s.T(), list.Items, attachment)
+		}
+	}
 }
