4313d885cfdeef9f4875864fe2ce3b6b7aaecef5
Robert Focke <robert_focke@yahoo.com>
2021-07-30 18:17:19 +0200

    COR-147: Attachment API (#128)
    * add attachments api server skeleton
    
    * fix small issues
    
    * add types and list options for attachments, implement store GET and LIST
    
    * add attachments endpoint to generic server, implement basic client for attachments
    
    * add rest handlers for LIST and GET, add server endpoints
    
    * add attachments server to generic server
    
    * data altering methods and validation
    
    * progress on testing
    
    * progress on testing, kudos to @nilueps for help with Goland error!
    
    * progress on testing
    
    * add new runConfiguration for attachments integration test
    
    * progress on getting things working
    
    * API tests passing, thanks for help catching sleepy errors @nilueps
    
    * cleaning up
    
    * add list filtering tests

---
+++ api/pkg/apps/attachments/post.go
@@ -0,0 +1,35 @@
+package attachments
+
+import (
+	"github.com/nrc-no/core/pkg/validation"
+	uuid "github.com/satori/go.uuid"
+	"net/http"
+)
+
+func (s *Server) PostAttachment(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+
+	var payload Attachment
+	if err := s.Bind(req, &payload); err != nil {
+		s.Error(w, err)
+		return
+	}
+
+	att := &payload
+
+	errList := ValidateAttachment(att, &validation.Path{})
+	if len(errList) > 0 {
+		status := errList.Status(http.StatusUnprocessableEntity, "invalid attachment")
+		s.Error(w, &status)
+		return
+	}
+
+	att.ID = uuid.NewV4().String()
+
+	if err := s.store.Create(ctx, att); err != nil {
+		s.Error(w, err)
+		return
+	}
+
+	s.json(w, http.StatusOK, att)
+}
