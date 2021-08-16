c3364fc744b74ce9ee3538430356944769e6af90
Nicolas Epstein <nilueps@gmail.com>
2021-08-16 15:15:40 -0400

    update to remove hooksPath if set and ensure hooks are executable

--- actions/install.sh
+++ actions/install.sh
@@ -3,10 +3,13 @@
 # move to script dir
 SRC_DIR="$(realpath "$(dirname "$0")")/git-hooks"
 GIT_DIR="$(realpath "$(dirname "$(dirname "$0")")")/.git"
+# remove hooksPath from git config if set
+grep -v "hooksPath" $GIT_DIR/config > __tmp && mv __tmp $GIT_DIR/config
 cd "$SRC_DIR" || exit
-
 for f in *; do
 	DEST="$GIT_DIR/hooks/$f"
+	# ensure hook is executable
+	chmod +x $f
 	# check if symlink or file exists
 	if [ -h "$DEST" ] || [ -f "$DEST" ]; then
 		echo "Removing old file."
