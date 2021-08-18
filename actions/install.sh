860d89a7451d0e262634388acec3a6428e37ea47
Nicolas Epstein <nilueps@gmail.com>
2021-08-18 15:36:44 -0400

    update install script to (hopefully) work on mac

--- actions/install.sh
+++ actions/install.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-# move to script dir
-SRC_DIR="$(realpath "$(dirname "$0")")/git-hooks"
-GIT_DIR="$(realpath "$(dirname "$(dirname "$0")")")/.git"
+if [ ! -e "install.sh" ]; then
+  echo "This script needs to be run from within its directory"
+  exit 1
+fi
+gitconfig='../.git/config'
 # remove hooksPath from git config if set
-grep -v "hooksPath" $GIT_DIR/config >__tmp && mv __tmp $GIT_DIR/config
-cd "$SRC_DIR" || exit
-for f in *; do
-  DEST="$GIT_DIR/hooks/$f"
-  # ensure hook is executable
-  chmod +x $f
-  # check if symlink or file exists
-  if [ -h "$DEST" ] || [ -f "$DEST" ]; then
-    echo "Removing old file."
-    rm "$DEST"
-  fi
-  # create symlink
-  if ln -s -f "$SRC_DIR/$f" "$DEST"; then
-    echo "Created symlink for $f hook."
-  fi
-done
+grep -v "hooksPath" $gitconfig > __tmp && mv __tmp $gitconfig
+# point hooksPath to hooks dir
+sed -i "s/\[core\]/\[core\]\n\thooksPath = actions\/git-hooks/" $gitconfig
+echo "Updated repo git config successfully."
