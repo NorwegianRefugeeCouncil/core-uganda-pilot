c758c57fc009063c1a321554631442c3e1fe6ac9
Robert Focke <robert_focke@yahoo.com>
2021-08-24 12:59:41 +0200

    progress on in-app pivot table

--- api/pkg/apps/webapp/reporting.go
+++ api/pkg/apps/webapp/reporting.go
@@ -1,9 +1,31 @@
 package webapp
 
-import "net/http"
+import (
+	"github.com/nrc-no/core/pkg/apps/iam"
+	"net/http"
+)
 
-func (h *Server) Reporting(w http.ResponseWriter, req *http.Request) {
-	if err := h.renderFactory.New(req).ExecuteTemplate(w, "reporting", map[string]interface{}{}); err != nil {
+func (s *Server) Reporting(w http.ResponseWriter, req *http.Request) {
+
+	iamClient, err := s.IAMClient(req)
+	if err != nil {
+		s.Error(w, err)
+		return
+	}
+
+	iList, err := iamClient.Individuals().List(req.Context(), iam.IndividualListOptions{})
+	if err != nil {
+		s.Error(w, err)
+		return
+	}
+
+	if err := s.renderFactory.New(req, w).ExecuteTemplate(w, "reporting", map[string]interface{}{
+		"Individuals":                   iList,
+		"GenderAttributeID":             iam.GenderAttribute.ID,
+		"DisplacementStatusAttributeID": iam.DisplacementStatusAttribute.ID,
+		"BirthDateAttributeID":          iam.BirthDateAttribute.ID,
+		"BeneficiaryPartyTypeID":        iam.BeneficiaryPartyType.ID,
+	}); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
