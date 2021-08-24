7f97e1c08a412aecee46e4b39f00aa64e40a89e0
Robert Focke <robert_focke@yahoo.com>
2021-08-24 13:47:48 +0200

    incorporating cases into data

--- api/pkg/apps/webapp/reporting.go
+++ api/pkg/apps/webapp/reporting.go
@@ -1,12 +1,14 @@
 package webapp
 
 import (
+	"github.com/nrc-no/core/pkg/apps/cms"
 	"github.com/nrc-no/core/pkg/apps/iam"
 	"net/http"
 )
 
 func (s *Server) Reporting(w http.ResponseWriter, req *http.Request) {
 
+	// get list of individuals
 	iamClient, err := s.IAMClient(req)
 	if err != nil {
 		s.Error(w, err)
@@ -19,8 +21,29 @@ func (s *Server) Reporting(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	// get list of cases and case types
+	cmsClient, err := s.CMSClient(req)
+	if err != nil {
+		s.Error(w, err)
+		return
+	}
+
+	cList, err := cmsClient.Cases().List(req.Context(), cms.CaseListOptions{})
+	if err != nil {
+		s.Error(w, err)
+		return
+	}
+
+	ctList, err := cmsClient.CaseTypes().List(req.Context(), cms.CaseTypeListOptions{})
+	if err != nil {
+		s.Error(w, err)
+		return
+	}
+
 	if err := s.renderFactory.New(req, w).ExecuteTemplate(w, "reporting", map[string]interface{}{
 		"Individuals":                   iList,
+		"Cases":                         cList,
+		"CaseTypes":                     ctList,
 		"GenderAttributeID":             iam.GenderAttribute.ID,
 		"DisplacementStatusAttributeID": iam.DisplacementStatusAttribute.ID,
 		"BirthDateAttributeID":          iam.BirthDateAttribute.ID,
