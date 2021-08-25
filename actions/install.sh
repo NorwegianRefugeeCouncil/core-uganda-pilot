b036cc41eba70c68ccf73283fd02909951859e0b
Nicolas Epstein <nilueps@gmail.com>
2021-08-25 08:18:00 -0400

    add condition for mac os

--- actions/install.sh
+++ actions/install.sh
@@ -4,9 +4,19 @@ if [ ! -e "install.sh" ]; then
   echo "This script needs to be run from within its directory"
   exit 1
 fi
+
 gitconfig='../.git/config'
+
 # remove hooksPath from git config if set
 grep -v "hooksPath" $gitconfig > __tmp && mv __tmp $gitconfig
+
 # point hooksPath to hooks dir
-sed -i'' "s/\[core\]/\[core\]\n\thooksPath = actions\/git-hooks/" $gitconfig
+uname=$(uname)
+if [ "$uname" == "Linux" ]; then
+	sed -i'' "s/\[core\]/\[core\]\n\thooksPath = actions\/git-hooks/" $gitconfig
+elif [ "$uname" == "Darwin" ]; then
+	# add a space after the '-i' flag on mac...
+	sed -i '' "s/\[core\]/\[core\]\n\thooksPath = actions\/git-hooks/" $gitconfig
+fi
+
 echo "Updated repo git config successfully."
