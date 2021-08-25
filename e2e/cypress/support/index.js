7b8d42db85e48fcfc178cfbea9a1faed60fd9312
Nicolas Epstein <nilueps@gmail.com>
2021-08-25 16:53:47 -0400

    add cleanup logic to remove dangling processes after hook

--- e2e/cypress/support/index.js
+++ e2e/cypress/support/index.js
@@ -21,9 +21,3 @@ import './commands';
 
 // call our custom login with access token before each test
 beforeEach(() => cy.login('courtney.lare@email.com'));
-afterEach(function() {
-  if (this.currentTest.state === 'failed') {
-    Cypress.runner.stop()
-  }
-});
-
