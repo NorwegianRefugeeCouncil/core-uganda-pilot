180fc265f934c05954e30ef0bd49105c632ca4dc
Robert Focke <robert_focke@yahoo.com>
2021-09-20 16:28:57 +0200

    land on client-side js chart switching, implement async requests on the server-side to get data

--- api/pkg/apps/webapp/reporting.go
+++ api/pkg/apps/webapp/reporting.go
@@ -3,39 +3,59 @@ package webapp
 import (
 	"github.com/nrc-no/core/pkg/apps/cms"
 	"github.com/nrc-no/core/pkg/apps/iam"
+	"golang.org/x/sync/errgroup"
 	"net/http"
 )
 
 func (s *Server) Reporting(w http.ResponseWriter, req *http.Request) {
 
-	// get list of individuals
+	var iList *iam.IndividualList
+	var cList *cms.CaseList
+	var ctList *cms.CaseTypeList
+
+	g, _ := errgroup.WithContext(req.Context())
+
+	// initialise clients
 	iamClient, err := s.IAMClient(req)
 	if err != nil {
 		s.Error(w, err)
 		return
 	}
 
-	iList, err := iamClient.Individuals().List(req.Context(), iam.IndividualListOptions{})
-	if err != nil {
-		s.Error(w, err)
-		return
-	}
-
-	// get list of cases and case types
 	cmsClient, err := s.CMSClient(req)
 	if err != nil {
 		s.Error(w, err)
 		return
 	}
 
-	cList, err := cmsClient.Cases().List(req.Context(), cms.CaseListOptions{})
-	if err != nil {
-		s.Error(w, err)
-		return
-	}
+	// get individuals
+	g.Go(func() error {
+		iList, err = iamClient.Individuals().List(req.Context(), iam.IndividualListOptions{})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
 
-	ctList, err := cmsClient.CaseTypes().List(req.Context(), cms.CaseTypeListOptions{})
-	if err != nil {
+	// get list of cases
+	g.Go(func() error {
+		cList, err = cmsClient.Cases().List(req.Context(), cms.CaseListOptions{})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	// get list of case types
+	g.Go(func() error {
+		ctList, err = cmsClient.CaseTypes().List(req.Context(), cms.CaseTypeListOptions{})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err := g.Wait(); err != nil {
 		s.Error(w, err)
 		return
 	}
