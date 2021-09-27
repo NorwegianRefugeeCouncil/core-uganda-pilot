d4324dff664581df87c8de7f46deb420d3771776
Robert Focke <robert.focke@member.fsf.org>
2021-09-27 17:54:51 +0200

    add go -> ts compilation

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"flag"
 	"github.com/nrc-no/core/cmd/app"
+	"github.com/nrc-no/core/pkg/apps/iam"
 	"github.com/nrc-no/core/pkg/server"
+	"github.com/tkrajina/typescriptify-golang-structs/typescriptify"
 )
 
 func main() {
@@ -12,6 +14,16 @@ func main() {
 	options := server.NewOptions()
 	cmd := app.LaunchCommand(ctx, options)
 	cmd.Flags().AddGoFlagSet(flag.CommandLine)
+
+	// Generate TS types for client
+	converter := typescriptify.New().
+		Add(iam.Party{})
+	err := converter.ConvertToFile("models.ts")
+	if err != nil {
+		panic(err.Error())
+	}
+	// end of TS type generation
+
 	if err := cmd.Execute(); err != nil {
 		panic(err)
 	}
