dee6d56b1ce5e13a2762a11b23989c4bcfe84caa
Nicolas Epstein <nilueps@gmail.com>
2021-09-29 08:28:15 -0400

    add form group struct

--- api/pkg/form/form.go
+++ api/pkg/form/form.go
@@ -8,6 +8,7 @@ import (
 // Form describes the content of an HTML form element as an ordered list of Control elements
 type Form struct {
 	Controls []Control `json:"controls" bson:"controls"`
+	Groups   []Group   `json:"groups" bson:"groups"`
 }
 
 // ValuedForm is a Form which has undergone validation. It is composed of an ordered list of ValuedControl as well as
