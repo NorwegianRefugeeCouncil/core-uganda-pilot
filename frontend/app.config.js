5808922b5de1e5adebc21804e6e57e6ba071cc97
Nicolas Epstein <nilueps@gmail.com>
2021-10-07 08:23:08 -0400

    add support for NODE_ENV variable

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -1,8 +1,25 @@
 import 'dotenv/config';
 
-export default {
-    extra: {
-        server_default_hostname: 'localhost:9000',
-        server_hostname: process.env.SERVER_HOSTNAME,
-    },
-};
+const config = setConfig();
+
+function setConfig() {
+	switch (process.env.NODE_ENV) {
+		case 'development':
+		case 'dev':
+			return {
+				extra: {
+					server_default_hostname: 'localhost:9000',
+					server_hostname: process.env.SERVER_HOSTNAME,
+				},
+			}
+		case 'production':
+		case 'prod':
+			return {}
+		case 'staging':
+			return {}
+		default:
+			return {}
+	}
+}
+
+export default config;
