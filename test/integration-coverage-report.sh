23729662dfd67018223cec6850884e85fb20e9c8
Nicolas Epstein <nilueps@gmail.com>
2021-10-11 17:27:18 -0400

    update run configs; update cypress

--- test/integration-coverage-report.sh
+++ test/integration-coverage-report.sh
@@ -7,6 +7,4 @@
 cd "$(dirname "$0")" || exit
 go test -coverprofile=coverage.out github.com/nrc-no/core/pkg/iam github.com/nrc-no/core/pkg/cms github.com/nrc-no/core/pkg/attachments
 go tool cover -html=coverage.out
-#go tool cover -func=coverage.out
-#go tool cover -html=coverage.out
 rm coverage.out
