f430289258eb57bb971f392d0cc498db296a10c0
Nicolas Epstein <nilueps@gmail.com>
2021-10-12 18:46:56 -0400

    move tsconfig to project root; adjust build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-tsc --build configs/tsconfig.json
+tsc --build tsconfig.json
 
 if [ ! "$?" ]; then
   exit 1
 fi
 
-echo ">>> Transpiled .ts"
+echo ">>> Transpiled typescript"
 
 go build -o ./tmp/main ./cmd/core
 
