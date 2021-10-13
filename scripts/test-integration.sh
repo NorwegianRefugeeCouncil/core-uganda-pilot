32c2aa6acf1bebf81be773842355c579aa010277
Nicolas Epstein <nilueps@gmail.com>
2021-10-13 14:59:57 -0400

    fix newline literal

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -5,18 +5,18 @@ go test github.com/nrc-no/core/pkg/iam
 if [ ! "$?" ]; then
   exit 1
 fi
-echo "\n"
+printf "\n"
 
 echo ">>> Testing package: cms"
 go test github.com/nrc-no/core/pkg/cms
 if [ ! "$?" ]; then
   exit 1
 fi
-echo "\n"
+printf "\n"
 
 echo ">>> Testing package: attachments"
 go test github.com/nrc-no/core/pkg/attachments
 if [ ! "$?" ]; then
   exit 1
 fi
-echo "\n"
+printf "\n"
