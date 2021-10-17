068395ccede8b9a57bf84a6c5cfb88a8f83232e4
Ludovic Cleroux <lcleroux@tempo.io>
2021-10-17 08:01:18 +0200

    COR-294 created bucket_validation.go

--- pkg/documents/bucket_create.go
+++ pkg/documents/bucket_create.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 	"github.com/nrc-no/core/pkg/storage"
 	"github.com/nrc-no/core/pkg/utils"
-	"github.com/nrc-no/core/pkg/validation"
 	"net/http"
-	"regexp"
 )
 
 func CreateBucket(
@@ -45,33 +43,4 @@ func CreateBucket(
 		utils.JSONResponse(w, http.StatusOK, bucket)
 
 	}
-}
-
-func validateBucket(fieldPath validation.Path, bucket *Bucket) validation.ErrorList {
-	result := validation.ErrorList{}
-	result = append(result, validateBucketName(fieldPath.Key("name"), bucket.Name)...)
-	return result
-}
-
-var bucketNameRegex = regexp.MustCompile("^[a-zA-Z0-9]+([_-][a-zA-Z0-9]+)*$")
-var bucketNameMinLength = 3
-var bucketNameMaxLength = 128
-
-func validateBucketName(fieldPath *validation.Path, name string) validation.ErrorList {
-	result := validation.ErrorList{}
-
-	if len(name) == 0 {
-		result = append(result, validation.Required(fieldPath, name))
-	}
-	if len(name) < bucketNameMinLength {
-		result = append(result, validation.TooShort(fieldPath, name, bucketNameMinLength))
-	}
-	if len(name) > bucketNameMaxLength {
-		result = append(result, validation.TooLong(fieldPath, name, bucketNameMaxLength))
-	}
-	if !bucketNameRegex.MatchString(name) {
-		result = append(result, validation.Invalid(fieldPath, name, "invalid character sequence"))
-	}
-
-	return result
 }
---
+++ pkg/documents/bucket_validation.go
@@ -0,0 +1,35 @@
+package documents
+
+import (
+	"github.com/nrc-no/core/pkg/validation"
+	"regexp"
+)
+
+func validateBucket(fieldPath validation.Path, bucket *Bucket) validation.ErrorList {
+	result := validation.ErrorList{}
+	result = append(result, validateBucketName(fieldPath.Key("name"), bucket.Name)...)
+	return result
+}
+
+var bucketNameRegex = regexp.MustCompile("^[a-zA-Z0-9]+([_-][a-zA-Z0-9]+)*$")
+var bucketNameMinLength = 3
+var bucketNameMaxLength = 128
+
+func validateBucketName(fieldPath *validation.Path, name string) validation.ErrorList {
+	result := validation.ErrorList{}
+
+	if len(name) == 0 {
+		result = append(result, validation.Required(fieldPath, name))
+	}
+	if len(name) < bucketNameMinLength {
+		result = append(result, validation.TooShort(fieldPath, name, bucketNameMinLength))
+	}
+	if len(name) > bucketNameMaxLength {
+		result = append(result, validation.TooLong(fieldPath, name, bucketNameMaxLength))
+	}
+	if !bucketNameRegex.MatchString(name) {
+		result = append(result, validation.Invalid(fieldPath, name, "invalid character sequence"))
+	}
+
+	return result
+}
