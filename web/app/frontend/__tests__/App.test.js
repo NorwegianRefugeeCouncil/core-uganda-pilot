b021e739a4285064f0813d626f852199ef0bb35f
Senyao Hou <senyao.hou@gmail.com>
2021-10-22 18:49:17 +0200

    setup jest testing framework for web app

---
+++ web/app/frontend/__tests__/App.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Renderer from 'react-test-renderer';
+import App from '../App.tsx';
+
+describe('App', () => {
+    
+    it('<App /> has 1 child', () => {
+      const tree = Renderer.create(<App />).toJSON();
+      console.log(tree)
+      expect(tree.children.length).toBe(1);
+    });
+
+    it('renders correctly', () => {
+        const tree = Renderer
+          .create(<App />)
+          .toJSON();
+        expect(tree).toMatchSnapshot();
+      });
+
+  });
