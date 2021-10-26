b2d7a398deacf7235d4a8d00193d347a6e12724d
Ludovic Cleroux <lcleroux@tempo.io>
2021-10-26 15:29:23 +0200

    Enhance startup scripts

--- scripts/down.sh
+++ scripts/down.sh
@@ -2,6 +2,10 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+# necessary if the secrets were removed for some reason
+# down would fail because env_file not found
+(cd $SCRIPT_DIR && ./init_secrets.sh)
+
 docker-compose -f deployments/webapp.docker-compose.yaml down --remove-orphans
 
-(cd $SCRIPT_DIR && ./reset_local_config.sh)
+(cd $SCRIPT_DIR && ./reset_secrets.sh)
--- scripts/up.sh
+++ scripts/up.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 set -e
 
-${SCRIPT_DIR}/init-local-config.sh
+${SCRIPT_DIR}/init_secrets.sh
 
 docker-compose -f deployments/webapp.docker-compose.yaml up --build -d
 
