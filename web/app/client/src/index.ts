6196b8e9b006795e4e74897be58049b4f94de5b2
Ludovic Cleroux <ludovic.cleroux@gmail.com>
2021-12-02 00:47:17 +0100

    COR-340 Fix client library build

--- web/app/client/src/index.ts
+++ web/app/client/src/index.ts
@@ -1,4 +1,4 @@
-import axios, {AxiosError, AxiosInstance, Method} from "axios";
+import axios, {AxiosInstance, Method} from "axios";
 import {
     ClientDefinition,
     DatabaseCreateRequest,
@@ -24,6 +24,7 @@ import {clientResponse} from "./utils/responses";
 
 export default class Client implements ClientDefinition {
     private corev1 = "apis/core.nrc.no/v1"
+
     constructor(
         public readonly address = 'https://core.dev:8443',
         public readonly axiosInstance: AxiosInstance = axios.create()) {
@@ -42,15 +43,6 @@ export default class Client implements ClientDefinition {
             withCredentials: true,
         }).then(value => {
             return clientResponse<TRequest, TBody>(value, request, expectStatusCode);
-        }).catch((err: AxiosError) => {
-            return {
-                request: request,
-                response: undefined,
-                status: err.response?.statusText,
-                statusCode: err.response?.status,
-                error: err.response,
-                success: false,
-            }
         })
     }
 
