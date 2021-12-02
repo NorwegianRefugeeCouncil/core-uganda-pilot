8f2d63318e0a52de27600d44501c52746f1261b5
Ludovic Cleroux <ludovic.cleroux@gmail.com>
2021-12-02 12:27:29 +0100

    Update recipientdef.go

--- pkg/api/types/recipientdef.go
+++ pkg/api/types/recipientdef.go
@@ -5,7 +5,7 @@ package types
 type RecipientDef struct {
 	FormDefinition
 
-	// Extends represents the Parent of the Recipient. Sometimes, we need to have multiple "profiles"
+	// Parent represents the Parent of the Recipient. Sometimes, we need to have multiple "profiles"
 	// for case recipients. For example, Uganda Beneficiary Individual, Kenya Beneficiary Individual,
 	// Colombia BeneficiaryIndividual are 3 different recipients that "extend" the same "Global Beneficiary Individual"
 	// In this case, Uganda Beneficiary Individual would have Global Beneficiary Individual as Parent
@@ -13,5 +13,5 @@ type RecipientDef struct {
 	// Also, in some cases, the parent might be a custom FormDefinition that is not a RecipientDef.
 	// If the user has already defined a custom free-form, eg. Individual, and wants to add Case Management
 	// to this currently existing form, then the Extends property would reference the Individual FormDefinition.
-	Extends *FormRef `json:"extends,omitempty"`
+	Parent *FormRef `json:"extends,omitempty"`
 }
