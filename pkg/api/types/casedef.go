18b136f0d9611a59effe7d66139f5e36732f5559
Ludovic Cleroux <ludovic.cleroux@gmail.com>
2021-12-03 10:19:55 +0100

    COR-363 PR comments

--- pkg/api/types/casedef.go
+++ pkg/api/types/casedef.go
@@ -1,11 +1,11 @@
 package types
 
-// CaseRecipientSpec represents the specification for allowed recipients of a CaseDefinition
+// CaseRecipientDefinition represents the specification for allowed recipients of a CaseDefinition
 // Introducing a struct here since it would be possible
 // to have recipients that are not records in other forms in the future.
 // For example, we might have a recipient that is a link to multiple people
 // (Extensibility)
-type CaseRecipientSpec struct {
+type CaseRecipientDefinition struct {
 	// FormRef represents a recipient that is a record in another form
 	FormRef *FormRef `json:"form,omitempty"`
 }
@@ -18,8 +18,8 @@ type CaseDefinition struct {
 	// data about this case.
 	// We embed the FormDefinition here since a Case is also a Form.
 	FormDefinition FormDefinition `json:"formSpec"`
-	// Recipients represents the types of recipient that this case allows.
+	// RecipientDefinitions represents the types of recipient that this case allows.
 	// For example, if the CaseDefinition is Colombia Individual Intake, then
-	// the Recipients could be a link to a ColombiaIndividualRecipient form.
-	Recipients []CaseRecipientSpec `json:"recipient,omitempty"`
+	// the RecipientDefinitions could be a link to a ColombiaIndividualRecipient form.
+	RecipientDefinitions []CaseRecipientDefinition `json:"recipient,omitempty"`
 }
