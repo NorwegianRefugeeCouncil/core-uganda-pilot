d3b08eb62bd533fb6233591b8dfc14388ef1dd7e
Nicolas Epstein <nilueps@gmail.com>
2021-12-03 11:47:59 -0500

    lint

--- web/app/frontend/__tests__/screen_containers/FormsScreenContainer.test.js
+++ web/app/frontend/__tests__/screen_containers/FormsScreenContainer.test.js
@@ -1,22 +1,22 @@
-import { render } from '@testing-library/react-native';
-import React from 'react';
-import { FormsScreenContainer } from '../../src/components/screen_containers/FormsScreenContainer';
+import { render } from "@testing-library/react-native";
+import React from "react";
+import { FormsScreenContainer } from "../../src/components/screen_containers/FormsScreenContainer";
 
 const mockNavigation = jest.fn();
-const mockResponse = { response: { items: []}};
+const mockResponse = { response: { items: [] } };
 const mockListForms = jest.fn(() => Promise.resolve(mockResponse));
 
-jest.mock('../../src/utils/clients', () => {
-  return () => ({
-    listForms: () => mockListForms(),
-  })
-})
+jest.mock("../../src/utils/clients", () => {
+    return () => ({
+        listForms: () => mockListForms(),
+    });
+});
 
 describe(FormsScreenContainer.name, () => {
-  beforeAll(() => {
-    render(<FormsScreenContainer navigation={mockNavigation} />);
-  })
-  test('fetches forms', () => {
-    expect(mockListForms).toHaveBeenCalled();
-  })
+    beforeAll(() => {
+        render(<FormsScreenContainer navigation={mockNavigation} />);
+    });
+    test("fetches forms", () => {
+        expect(mockListForms).toHaveBeenCalled();
+    });
 });
--- web/app/frontend/src/components/screen_containers/FormsScreenContainer.tsx
+++ web/app/frontend/src/components/screen_containers/FormsScreenContainer.tsx
@@ -5,23 +5,14 @@ import { FormsScreenContainerProps } from "../../types/screens";
 import client from "../../utils/clients";
 import { FormsScreen } from "../screens/FormsScreen";
 
-export const FormsScreenContainer = ({
-    navigation,
-    route,
-}: FormsScreenContainerProps) => {
+export const FormsScreenContainer = ({ navigation, route }: FormsScreenContainerProps) => {
     const [forms, setForms] = React.useState<FormDefinition[]>();
     const [isLoading, setIsLoading] = React.useState(true);
     React.useEffect(() => {
-        client.listForms({}).then((data) => {
+        client.listForms({}).then(data => {
             setIsLoading(false);
             setForms(data.response?.items);
         });
     });
-    return (
-        <FormsScreen
-            isLoading={isLoading}
-            forms={forms}
-            navigation={navigation}
-        />
-    );
+    return <FormsScreen isLoading={isLoading} forms={forms} navigation={navigation} />;
 };
