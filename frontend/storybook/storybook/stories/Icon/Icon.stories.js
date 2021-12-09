03baab01e6849bdc35e640fefe14f1967c3595a5
Ben McAlindin <bmcalindin@gmail.com>
2021-12-09 16:15:40 +0000

    Fix import path

--- frontend/storybook/storybook/stories/Icon/Icon.stories.js
+++ frontend/storybook/storybook/stories/Icon/Icon.stories.js
@@ -1,26 +1,22 @@
-import {storiesOf} from '@storybook/react-native';
+import { storiesOf } from '@storybook/react-native';
 import React from 'react';
-import {Icon} from 'core-design-system'
-import {IconName, IconVariants} from "core-design-system/lib/types/icons";
-import CenterView from "../CenterView";
-import {select} from "@storybook/addon-knobs";
+import { Icon } from 'core-design-system';
+import { IconName, IconVariants } from 'core-design-system/lib/esm/types/icons';
+import { select } from '@storybook/addon-knobs';
+
+import CenterView from '../CenterView';
 
 storiesOf('Icon', module)
-    .addDecorator((getStory) =>
-        <CenterView>
-            {getStory()}
-        </CenterView>
-    )
-    .add('Icon', () => {
-        const IconNameList = Object.entries(IconName)
-            .map(([_, value]) => (value));
-        const IconVariantList = Object.entries(IconVariants)
-            .map(([_, value]) => (value));
+  .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
+  .add('Icon', () => {
+    const IconNameList = Object.entries(IconName).map(([_, value]) => value);
+    const IconVariantList = Object.entries(IconVariants).map(([_, value]) => value);
 
-        return (
-            <Icon
-                name={select('icon name', IconNameList, IconName.ATTACHMENT)}
-                variant={select('variant', IconVariantList, IconVariants.DARK)}
-            />
-        )
-    });
+    console.log(IconNameList, IconVariantList);
+    return (
+      <Icon
+        name={select('icon name', IconNameList, IconName.ATTACHMENT)}
+        variant={select('variant', IconVariantList, IconVariants.DARK)}
+      />
+    );
+  });
