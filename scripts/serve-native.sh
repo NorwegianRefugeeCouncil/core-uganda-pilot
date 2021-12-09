5796bef4ea897ddf1362f90bb61cbb13ede236a7
Ben McAlindin <bmcalindin@gmail.com>
2021-12-09 17:05:50 +0000

    Don't use lerna to start native apps as it doesn't open the url

--- scripts/serve-native.sh
+++ scripts/serve-native.sh
@@ -2,5 +2,5 @@
 set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-(cd "${SCRIPT_DIR}/../frontend" && yarn start:pwa)
+(cd "${SCRIPT_DIR}/../frontend/apps/native" && yarn start)
 
