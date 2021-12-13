a879ab125cb047e99bece639d03e93872b99a579
Ludovic Cleroux <ludovic.cleroux@gmail.com>
2021-12-13 11:22:14 +0100

    COR-418 fix remove forgotten ctx

--- cmd/serve_bouncer.go
+++ cmd/serve_bouncer.go
@@ -29,7 +29,7 @@ func init() {
 }
 
 func serveAuthnzBouncer(ctx context.Context, options auth.Options) error {
-	server, err := auth.NewServer(ctx, options)
+	server, err := auth.NewServer(options)
 	if err != nil {
 		return err
 	}
