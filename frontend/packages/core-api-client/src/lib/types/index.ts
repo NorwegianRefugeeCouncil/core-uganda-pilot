30b8d7cac02fd302ef87af5734f3c8656b8513fe
Ludovic Cleroux <ludovic.cleroux@gmail.com>
2021-12-14 14:53:32 +0100

    add types in frontend api client package

--- frontend/packages/core-api-client/src/lib/types/index.ts
+++ frontend/packages/core-api-client/src/lib/types/index.ts
@@ -1,36 +1,38 @@
 import {AxiosInstance} from "axios";
 
 export type Database = {
-    id: string
-    name: string
+  id: string
+  name: string
 }
 
 export type DatabaseList = {
-    items: Database[]
+  items: Database[]
 }
 
 export enum FieldKind {
-    Text = "text",
-    MultilineText = "multilineText",
-    Reference = "reference",
-    SubForm = "subform",
-    Date = "date",
-    Quantity = "quantity",
-    SingleSelect = "singleSelect",
-    Week = "week",
-    Month = "month"
+  Text = "text",
+  MultilineText = "multilineText",
+  Reference = "reference",
+  SubForm = "subform",
+  Date = "date",
+  Quantity = "quantity",
+  SingleSelect = "singleSelect",
+  MultiSelect = "multiSelect",
+  Week = "week",
+  Month = "month"
 }
 
 export type FieldType = {
-    text?: FieldTypeText
-    reference?: FieldTypeReference
-    subForm?: FieldTypeSubForm
-    multilineText?: FieldTypeMultilineText
-    date?: FieldTypeDate
-    month?: FieldTypeMonth
-    week?: FieldTypeWeek
-    quantity?: FieldTypeQuantity
-    singleSelect?: FieldTypeSingleSelect
+  text?: FieldTypeText
+  reference?: FieldTypeReference
+  subForm?: FieldTypeSubForm
+  multilineText?: FieldTypeMultilineText
+  date?: FieldTypeDate
+  month?: FieldTypeMonth
+  week?: FieldTypeWeek
+  quantity?: FieldTypeQuantity
+  singleSelect?: FieldTypeSingleSelect
+  multiSelect?: FieldTypeMultiSelect
 }
 
 export type FieldTypeText = {}
@@ -44,87 +46,91 @@ export type FieldTypeMonth = {}
 export type FieldTypeQuantity = {}
 
 export type SelectOption = {
-    name: string
-    id: string
+  name: string
+  id: string
 }
 
 export type FieldTypeSingleSelect = {
-    options: SelectOption[]
+  options: SelectOption[]
+}
+
+export type FieldTypeMultiSelect = {
+  options: SelectOption[]
 }
 
 export class FieldTypeWeek {
 }
 
 export type FieldTypeReference = {
-    databaseId: string
-    formId: string
+  databaseId: string
+  formId: string
 }
 
 export type FieldTypeSubForm = {
-    fields: FieldDefinition[]
+  fields: FieldDefinition[]
 }
 
 export type FieldDefinition = {
-    id: string
-    code: string
-    name: string
-    description: string
-    required: boolean
-    key: boolean
-    fieldType: FieldType
+  id: string
+  code: string
+  name: string
+  description: string
+  required: boolean
+  key: boolean
+  fieldType: FieldType
 }
 
 export type FormDefinition = {
-    id: string
-    code: string
-    databaseId: string
-    folderId: string
-    name: string
-    fields: FieldDefinition[]
+  id: string
+  code: string
+  databaseId: string
+  folderId: string
+  name: string
+  fields: FieldDefinition[]
 }
 
 export type FormDefinitionList = {
-    items: FormDefinition[]
+  items: FormDefinition[]
 }
 
 export type Folder = {
-    id: string
-    databaseId: string
-    parentId: string
-    name: string
+  id: string
+  databaseId: string
+  parentId: string
+  name: string
 }
 
 export type FolderList = {
-    items: Folder[]
+  items: Folder[]
 }
 
 export type FieldValue = {
-    fieldId: string
-    value: string
+  fieldId: string
+  value: string | string[] | null
 }
 
 export type Record = {
-    id: string
-    databaseId: string
-    formId: string
-    ownerId: string | undefined
-    values: FieldValue[]
+  id: string
+  databaseId: string
+  formId: string
+  ownerId: string | undefined
+  values: FieldValue[]
 }
 
 export type LocalRecord = Record & {
-    isNew: boolean
+  isNew: boolean
 }
 
 export type RecordList = { items: Record[] }
 
 
 export type Response<TRequest, TResponse> = {
-    request: TRequest
-    response: TResponse | undefined
-    status: string
-    statusCode: number
-    success: boolean
-    error: any
+  request: TRequest
+  response: TResponse | undefined
+  status: string
+  statusCode: number
+  success: boolean
+  error: any
 }
 
 export type PartialObjectWrapper<T> = { object: Partial<T> }
@@ -134,14 +140,14 @@ export type DatabaseCreateRequest = PartialObjectWrapper<Database>
 export type DatabaseCreateResponse = Response<DatabaseCreateRequest, Database>
 
 export interface DatabaseCreator {
-    createDatabase: DataOperation<DatabaseCreateRequest, DatabaseCreateResponse>
+  createDatabase: DataOperation<DatabaseCreateRequest, DatabaseCreateResponse>
 }
 
 export type DatabaseListRequest = {} | undefined
 export type DatabaseListResponse = Response<DatabaseListRequest, DatabaseList>
 
 export interface DatabaseLister {
-    listDatabases: DataOperation<DatabaseListRequest, DatabaseListResponse>
+  listDatabases: DataOperation<DatabaseListRequest, DatabaseListResponse>
 }
 
 export type FormListRequest = {} | undefined
@@ -151,67 +157,67 @@ export type FormGetRequest = { id: string }
 export type FormGetResponse = Response<FormGetRequest, FormDefinition>
 
 export interface FormGetter {
-    getForm: DataOperation<FormGetRequest, FormGetResponse>
+  getForm: DataOperation<FormGetRequest, FormGetResponse>
 }
 
 export interface FormLister {
-    listForms: DataOperation<FormListRequest, FormListResponse>
+  listForms: DataOperation<FormListRequest, FormListResponse>
 }
 
 export type FormCreateRequest = PartialObjectWrapper<FormDefinition>
 export type FormCreateResponse = Response<FormCreateRequest, FormDefinition>
 
 export interface FormCreator {
-    createForm: DataOperation<FormCreateRequest, FormCreateResponse>
+  createForm: DataOperation<FormCreateRequest, FormCreateResponse>
 }
 
 export type RecordCreateRequest = PartialObjectWrapper<Record>
 export type RecordCreateResponse = Response<RecordCreateRequest, Record>
 
 export interface RecordCreator {
-    createRecord: DataOperation<RecordCreateRequest, RecordCreateResponse>
+  createRecord: DataOperation<RecordCreateRequest, RecordCreateResponse>
 }
 
 export type FolderListRequest = {} | undefined
 export type FolderListResponse = Response<FolderListRequest, FolderList>
 
 export interface FolderLister {
-    listFolders: DataOperation<FolderListRequest, FolderListResponse>
+  listFolders: DataOperation<FolderListRequest, FolderListResponse>
 }
 
 export type FolderCreateRequest = PartialObjectWrapper<Folder>
 export type FolderCreateResponse = Response<FolderCreateRequest, Folder>
 
 export interface FolderCreator {
-    createFolder: DataOperation<FolderCreateRequest, FolderCreateResponse>
+  createFolder: DataOperation<FolderCreateRequest, FolderCreateResponse>
 }
 
 export type RecordListRequest = { databaseId: string, formId: string }
 export type RecordListResponse = Response<RecordListRequest, RecordList>
 
 export interface RecordLister {
-    listRecords: DataOperation<RecordListRequest, RecordListResponse>
+  listRecords: DataOperation<RecordListRequest, RecordListResponse>
 }
 
 export type RecordGetRequest = { databaseId: string, formId: string, recordId: string }
 export type RecordGetResponse = Response<RecordGetRequest, Record>
 
 export interface RecordGetter {
-    getRecord: DataOperation<RecordGetRequest, RecordGetResponse>
+  getRecord: DataOperation<RecordGetRequest, RecordGetResponse>
 }
 
 
 export interface ClientDefinition
-    extends DatabaseCreator,
-        DatabaseLister,
-        FormLister,
-        FormGetter,
-        FormCreator,
-        RecordCreator,
-        RecordLister,
-        RecordGetter,
-        FolderLister,
-        FolderCreator {
-    address: string
-    axiosInstance: AxiosInstance
+  extends DatabaseCreator,
+    DatabaseLister,
+    FormLister,
+    FormGetter,
+    FormCreator,
+    RecordCreator,
+    RecordLister,
+    RecordGetter,
+    FolderLister,
+    FolderCreator {
+  address: string
+  axiosInstance: AxiosInstance
 }
