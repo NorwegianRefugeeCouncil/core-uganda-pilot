fe4a005dfa41b844b806c640c88e7aa0c88a38d1
Ludovic Cleroux <ludovic.cleroux@gmail.com>
2021-12-15 17:32:53 +0100

    fix cmd

--- cmd/serve_formsapi.go
+++ cmd/serve_formsapi.go
@@ -2,20 +2,20 @@ package cmd
 
 import (
 	"context"
-	"github.com/nrc-no/core/pkg/server/formsapi"
+	formsapiserver "github.com/nrc-no/core/pkg/server/formsapi"
 	"github.com/spf13/cobra"
 )
 
 // servePublicCmd represents the public command
 var servePublicCmd = &cobra.Command{
-	Use:   "forms",
+	Use:   "forms-api",
 	Short: "starts the public server",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if err := initStoreFactory(); err != nil {
 			return err
 		}
 		if err := servePublic(ctx,
-			formsapi.Options{
+			formsapiserver.Options{
 				ServerOptions: coreOptions.Serve.Public,
 				StoreFactory:  factory,
 			}); err != nil {
@@ -30,8 +30,8 @@ func init() {
 	serveCmd.AddCommand(servePublicCmd)
 }
 
-func servePublic(ctx context.Context, options formsapi.Options) error {
-	server, err := formsapi.NewServer(options)
+func servePublic(ctx context.Context, options formsapiserver.Options) error {
+	server, err := formsapiserver.NewServer(options)
 	if err != nil {
 		return err
 	}
