9f935b282d558c8f07166332be698243666d8921
Ludovic Cleroux <ludovic.cleroux@gmail.com>
2021-12-16 09:57:29 +0100

    Change single select store implementation

--- pkg/sqlmanager/schema_test.go
+++ pkg/sqlmanager/schema_test.go
@@ -329,10 +329,7 @@ create table "databaseId"."subFormField"(
 			}},
 			want: []schema.DDL{
 				{Query: createTableDDL},
-				{Query: `create table "databaseId"."singleSelectField_options"( "id" varchar(36) primary key, "name" varchar(128) not null unique);`},
-				{Query: `insert into "databaseId"."singleSelectField_options" ("id","name") values ($1,$2);`, Args: []interface{}{"option1", "Option 1"}},
-				{Query: `insert into "databaseId"."singleSelectField_options" ("id","name") values ($1,$2);`, Args: []interface{}{"option2", "Option 2"}},
-				{Query: `alter table "databaseId"."formId" add "singleSelectField" varchar(36) references "databaseId"."singleSelectField_options" ("id");`},
+				{Query: `alter table "databaseId"."formId" add "singleSelectField" varchar(36) check ("singleSelectField" IN ('option1','option2'));`},
 			},
 		},
 	}
