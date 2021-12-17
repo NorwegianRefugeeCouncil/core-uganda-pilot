65dce33c66c1f1bac89d0d6724ac2a39e62f28d4
internetti <irena.laemmerer@gmail.com>
2021-12-17 11:01:24 +0100

    DS-43: switch to camel case

--- frontend/packages/design-system/src/tokens/spacing.ts
+++ frontend/packages/design-system/src/tokens/spacing.ts
@@ -1,9 +1,9 @@
 export default {
-  'spacing-5': '5px',
-  'spacing-10': '10px',
-  'spacing-15': '15px',
-  'spacing-20': '20px',
-  'spacing-25': '25px',
-  'spacing-30': '30px',
-  'spacing-40': '40px',
+  spacing5: '5px',
+  spacing10: '10px',
+  spacing15: '15px',
+  spacing20: '20px',
+  spacing25: '25px',
+  spacing30: '30px',
+  spacing40: '40px',
 };
