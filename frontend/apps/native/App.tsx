5e6b6eb1ca5e52601b63dde8bdb98021b7ea3976
Ben McAlindin <bmcalindin@gmail.com>
2021-12-20 11:48:44 +0000

    Update native AuthWrapper to use an onTokenChange prop

--- frontend/apps/native/App.tsx
+++ frontend/apps/native/App.tsx
@@ -6,6 +6,7 @@ import theme from './src/constants/theme';
 import Router from './src/components/Router';
 import { AuthWrapper } from './src/components/AuthWrapper';
 import { ErrorBoundary } from './src/components/ErrorBoundary';
+import { apiClient } from './src/utils/clients';
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -13,7 +14,7 @@ export default function App() {
   return (
     <PaperProvider theme={theme}>
       <ErrorBoundary>
-        <AuthWrapper>
+        <AuthWrapper onTokenChange={apiClient.setAuth}>
           <Router />
         </AuthWrapper>
       </ErrorBoundary>
