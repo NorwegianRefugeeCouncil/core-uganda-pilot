b45031f7bc67379fd14c222f8d1ed4ff75c921b7
Nicolas Epstein <nilueps@gmail.com>
2021-12-21 10:08:11 -0500

    gofmt

--- pkg/seeder/colombia_dropdown_options.go
+++ pkg/seeder/colombia_dropdown_options.go
@@ -3,19 +3,20 @@ package seeder
 import "github.com/nrc-no/core/pkg/api/types"
 
 type Options = []*types.SelectOption
+
 var (
-  co_nationality1 = Options{}
-  co_nationality2 = Options{}
-  co_marital_status = Options{}
-  relationship_to_hh = Options{}
-  co_beneficiary_type = Options{}
-  co_ethnicity = Options{}
-  co_identification_type = Options{}
-  co_identification_source = Options{}
-  co_countries = Options{}
-  co_admin_2 = Options{}
-  co_settlement_type = Options{}
-  co_means_of_contact = Options{}
-  co_service_delivery_modalities = Options{}
-  co_intro_source = Options{}
+	co_nationality1                = Options{}
+	co_nationality2                = Options{}
+	co_marital_status              = Options{}
+	relationship_to_hh             = Options{}
+	co_beneficiary_type            = Options{}
+	co_ethnicity                   = Options{}
+	co_identification_type         = Options{}
+	co_identification_source       = Options{}
+	co_countries                   = Options{}
+	co_admin_2                     = Options{}
+	co_settlement_type             = Options{}
+	co_means_of_contact            = Options{}
+	co_service_delivery_modalities = Options{}
+	co_intro_source                = Options{}
 )
