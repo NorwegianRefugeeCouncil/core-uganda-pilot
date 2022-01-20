ef057cffa1ea6d09c82f36acdee6d3fe2b311a1e
Ben McAlindin <bmcalindin@gmail.com>
2022-01-20 17:02:28 +0100

    Edit boolean field in record in ui

---
+++ frontend/apps/pwa/src/components/formFields/BooleanFieldEditor.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+import { FieldEditorProps } from './types';
+import { FieldDescription } from './FieldDescription';
+
+export const BooleanFieldEditor: React.FC<FieldEditorProps> = ({
+  field,
+  value,
+  onChange,
+}) => {
+  return (
+    <div className="form-group mb-2">
+      <div className="form-check">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          checked={value === 'true'}
+          onChange={(event) =>
+            onChange(event.target.checked ? 'true' : 'false')
+          }
+          id={field.id}
+        />
+        <label className="form-check-label opacity-75" htmlFor={field.id}>
+          {field.name}
+        </label>
+      </div>
+      <FieldDescription text={field.description} />
+    </div>
+  );
+};
