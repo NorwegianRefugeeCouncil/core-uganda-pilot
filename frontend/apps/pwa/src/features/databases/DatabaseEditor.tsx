a06155d19ce5850dd4195948c9fdf0503124c9c6
internetti <irena.laemmerer@gmail.com>
2022-01-25 10:35:59 +0100

    COR-397: add validation to folders and databases

--- frontend/apps/pwa/src/features/databases/DatabaseEditor.tsx
+++ frontend/apps/pwa/src/features/databases/DatabaseEditor.tsx
@@ -2,9 +2,11 @@ import React, { FC, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Database } from 'core-api-client';
 import { Redirect } from 'react-router-dom';
+import _ from 'lodash';
 
 import { databaseActions } from '../../reducers/database';
 import client from '../../app/client';
+import { ApiErrorDetails } from '../../types/errors';
 
 type FormData = {
   name: string;
@@ -13,13 +15,15 @@ type FormData = {
 export const DatabaseEditor: FC = (props) => {
   const { register, handleSubmit } = useForm<FormData>();
   const [database, setDatabase] = useState<Database | undefined>(undefined);
+  const [errors, setErrors] = useState<ApiErrorDetails[] | undefined>();
 
   const onSubmit = (data: FormData) => {
     client.createDatabase({ object: { name: data.name } }).then((resp) => {
-      if (resp.response) {
+      if (resp.success && resp.response) {
         databaseActions.addOne(resp.response);
         setDatabase(resp.response);
-      } else {
+      } else if (!resp.success && resp.error) {
+        setErrors(resp.error.details.causes);
       }
     });
   };
@@ -35,7 +39,23 @@ export const DatabaseEditor: FC = (props) => {
             <form onSubmit={handleSubmit(onSubmit)}>
               <div className="form-group mb-2">
                 <label htmlFor="name">Database Name</label>
-                <input className="form-control" {...register('name')} />
+                <input
+                  {...register('name')}
+                  className={`form-control ${errors ? 'is-invalid' : ''}`}
+                  id="name"
+                  aria-describedby="nameFeedback"
+                />
+                {errors?.map((error) => {
+                  return (
+                    <div
+                      className="invalid-feedback is-invalid"
+                      id="nameFeedback"
+                      key={`${error.field}_${error.reason}`}
+                    >
+                      {error.message}
+                    </div>
+                  );
+                })}
               </div>
               <button className="btn btn-primary">Create New Database</button>
             </form>
