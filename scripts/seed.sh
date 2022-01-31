068317f5e8fcf431b5700b5a699ff0a6777ce945
Ludovic Cleroux <ludovic.cleroux@gmail.com>
2022-01-31 13:46:37 +0100

    COR-451 PR comments

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -7,7 +7,7 @@ set -e
 # cleanup on exit
 trap 'jobs -p | xargs -r kill' EXIT
 
-(cd "${SCRIPT_DIR}/.." && go run . seed global --endpoint https://localhost:9000 --config=configs/config.yaml,creds/core/config.yaml)
-(cd "${SCRIPT_DIR}/.." && go run . seed colombia --endpoint https://localhost:9000 --config=configs/config.yaml,creds/core/config.yaml)
+(cd "${SCRIPT_DIR}/.." && go run . dev seed global --endpoint https://localhost:9000 --config=configs/config.yaml,creds/core/config.yaml)
+(cd "${SCRIPT_DIR}/.." && go run . dev seed colombia --endpoint https://localhost:9000 --config=configs/config.yaml,creds/core/config.yaml)
 
 
