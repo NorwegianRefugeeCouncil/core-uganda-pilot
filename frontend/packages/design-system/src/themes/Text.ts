2a2978eb2b4b1d1d7568d1c38a7096a9c22793f8
Ben McAlindin <bmcalindin@gmail.com>
2022-02-23 14:49:38 +0100

    Use theme values for text rather than importing tokens. Use fontSize instead of variants

--- frontend/packages/design-system/src/themes/Text.ts
+++ frontend/packages/design-system/src/themes/Text.ts
@@ -1,97 +1,28 @@
-import typography from '../tokens/typography';
+const makeTextVariant =
+  (styles: Record<string, any>) => (props: Record<string, any>) => ({
+    lineHeight: props.fontSize,
+    ...styles,
+  });
 
 const textTheme = {
   baseStyle: {
     textAlign: 'auto',
     color: 'neutral.500',
+    fontFamily: 'body',
+    fontStyle: 'normal',
   },
   variants: {
-    display1: {
-      fontFamily: typography.fontConfig.Roboto['700'].normal,
-      fontSize: typography.fontSizes['3xl'],
-      lineHeight: typography.lineHeights['4xl'],
-    },
-    display2: {
-      fontFamily: typography.fontConfig.Roboto['700'].normal,
-      fontSize: typography.fontSizes['2xl'],
-      lineHeight: typography.lineHeights['3xl'],
-    },
-    heading1: {
-      fontFamily: typography.fontConfig.Roboto['400'].normal,
-      fontSize: typography.fontSizes.xl,
-      lineHeight: typography.lineHeights['2xl'],
-    },
-    heading2: {
-      fontFamily: typography.fontConfig.Roboto['400'].normal,
-      fontSize: typography.fontSizes.lg,
-      lineHeight: typography.lineHeights.xl,
-    },
-    heading3: {
-      fontFamily: typography.fontConfig.Roboto['400'].normal,
-      fontSize: typography.fontSizes.md,
-      lineHeight: typography.lineHeights.lg,
-    },
-    heading4: {
-      fontFamily: typography.fontConfig.Roboto['400'].normal,
-      fontSize: typography.fontSizes.sm,
-      lineHeight: typography.lineHeights.sm,
-    },
-    heading5: {
-      fontFamily: typography.fontConfig.Roboto['400'].normal,
-      fontSize: typography.fontSizes['3xs'],
-      lineHeight: typography.lineHeights['3xs'],
-    },
-    title1: {
-      fontFamily: typography.fontConfig.Roboto['400'].normal,
-      fontSize: typography.fontSizes.md,
-      lineHeight: typography.lineHeights.xl,
-    },
-    title2: {
-      fontFamily: typography.fontConfig.Roboto['400'].normal,
-      fontSize: typography.fontSizes.sm,
-      lineHeight: typography.lineHeights.lg,
-    },
-    title3: {
-      fontFamily: typography.fontConfig.Roboto['400'].normal,
-      fontSize: typography.fontSizes['2xs'],
-      lineHeight: typography.lineHeights.xs,
-    },
-    body1: {
-      fontFamily: typography.fontConfig.Roboto['400'].normal,
-      fontSize: typography.fontSizes.xs,
-      lineHeight: typography.lineHeights.md,
-    },
-    body2: {
-      fontFamily: typography.fontConfig.Roboto['400'].normal,
-      fontSize: typography.fontSizes['2xs'],
-      lineHeight: typography.lineHeights.xs,
-    },
-    inline: {
-      fontFamily: typography.fontConfig.Roboto['500'].medium,
-      fontSize: typography.fontSizes.xs,
-      lineHeight: typography.lineHeights.md,
+    display: makeTextVariant({ fontWeight: 'bold' }),
+    heading: makeTextVariant({ fontWeight: 'medium' }),
+    title: makeTextVariant({ fontWeight: 'regular' }),
+    body: makeTextVariant({ fontWeight: 'regular' }),
+    inline: makeTextVariant({
+      fontWeight: 'medium',
       textDecorationLine: 'underline',
-    },
-    date1: {
-      fontFamily: typography.fontConfig.Roboto['400'].italic,
-      fontSize: typography.fontSizes.xs,
-      lineHeight: typography.lineHeights.md,
-    },
-    date2: {
-      fontFamily: typography.fontConfig.Roboto['400'].italic,
-      fontSize: typography.fontSizes['2xs'],
-      lineHeight: typography.lineHeights.md,
-    },
-    label: {
-      fontFamily: typography.fontConfig.Roboto['500'].medium,
-      fontSize: typography.fontSizes.xs,
-      lineHeight: typography.lineHeights.md,
-    },
-    caption: {
-      fontFamily: typography.fontConfig.Roboto['400'].normal,
-      fontSize: typography.fontSizes['3xs'],
-      lineHeight: typography.lineHeights['2xs'],
-    },
+    }),
+    date: makeTextVariant({ fontWeight: 'regular', fontStyle: 'italic' }),
+    label: makeTextVariant({ fontWeight: 'medium' }),
+    caption: makeTextVariant({ fontWeight: 'regular' }),
   },
 };
 
