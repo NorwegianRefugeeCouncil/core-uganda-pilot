1cdfaabe87fe3db12d096e8cbb595b23cdafdae2
Ben McAlindin <bmcalindin@gmail.com>
2022-02-23 14:50:15 +0100

    Update text stories

--- frontend/storybook/storybook/stories/Text/Text.stories.js
+++ frontend/storybook/storybook/stories/Text/Text.stories.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { storiesOf } from '@storybook/react-native';
 import { VStack, Text } from 'native-base';
 
@@ -8,22 +7,56 @@ storiesOf('Text', module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
   .add('Overview, Variants', () => (
     <VStack space={2}>
-      <Text variant="display1">Display 1</Text>
-      <Text variant="display2">Display 2</Text>
-      <Text variant="heading1">Heading 1</Text>
-      <Text variant="heading2">Heading 2</Text>
-      <Text variant="heading3">Heading 3</Text>
-      <Text variant="heading4">Heading 4</Text>
-      <Text variant="heading5">Heading 5</Text>
-      <Text variant="title1">Title 1</Text>
-      <Text variant="title2">Title 2</Text>
-      <Text variant="title3">Title 3</Text>
-      <Text variant="body1">Body 1</Text>
-      <Text variant="body2">Body 2</Text>
-      <Text variant="caption">Caption</Text>
-      <Text variant="inline">Inline</Text>
-      <Text variant="date1">Date 1</Text>
-      <Text variant="date2">Date 2</Text>
-      <Text variant="label">Label</Text>
+      <Text variant="display" fontSize="3xl">
+        Display
+      </Text>
+      <Text variant="display" fontSize="2xl">
+        Display
+      </Text>
+      <Text variant="heading" fontSize="xl">
+        Heading
+      </Text>
+      <Text variant="heading" fontSize="lg">
+        Heading
+      </Text>
+      <Text variant="heading" fontSize="md">
+        Heading
+      </Text>
+      <Text variant="heading" fontSize="sm">
+        Heading
+      </Text>
+      <Text variant="heading" fontSize="3xs">
+        Heading
+      </Text>
+      <Text variant="title" fontSize="md">
+        Title
+      </Text>
+      <Text variant="title" fontSize="sm">
+        Title
+      </Text>
+      <Text variant="title" fontSize="2xs">
+        Title
+      </Text>
+      <Text variant="body" fontSize="xs">
+        Body
+      </Text>
+      <Text variant="body" fontSize="2xs">
+        Body
+      </Text>
+      <Text variant="caption" fontSize="3xs">
+        Caption
+      </Text>
+      <Text variant="inline" fontSize="xs">
+        Inline
+      </Text>
+      <Text variant="date" fontSize="xs">
+        Date
+      </Text>
+      <Text variant="date" fontSize="2xs">
+        Date
+      </Text>
+      <Text variant="label" fontSize="xs">
+        Label
+      </Text>
     </VStack>
   ));
