8090da9b09142c70ad742745bfe7895bdcdd5f64
internetti <irena.laemmerer@gmail.com>
2022-02-28 17:17:00 +0100

    COR-490: clean up index file and yarn lock file

--- frontend/packages/design-system/src/index.ts
+++ frontend/packages/design-system/src/index.ts
@@ -3,5 +3,4 @@ import { iconMap } from './assets/iconMap';
 export { default as theme } from './themes';
 export { default as tokens } from './tokens';
 export * from './components';
-// export { Accordion } from './components/Accordion';
 export const icons = iconMap;
