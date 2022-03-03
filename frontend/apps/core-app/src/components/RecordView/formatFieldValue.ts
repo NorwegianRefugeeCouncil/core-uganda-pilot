43907c9ef7c2905bfd169a36c101be8c06fa5dc8
Ben McAlindin <bmcalindin@gmail.com>
2022-03-03 11:11:57 +0100

    Add tests for formatFieldValue

--- frontend/apps/core-app/src/components/RecordView/formatFieldValue.ts
+++ frontend/apps/core-app/src/components/RecordView/formatFieldValue.ts
@@ -1,23 +1,24 @@
 import { FieldDefinition, FieldKind, getFieldKind } from 'core-api-client';
 
+// This method takes a field value and returns a formatted string
+// It handles extra cases, like a multi-select field value that is not an array, for safety
 export const formatFieldValue = (
   value: string | string[] | null,
   field: FieldDefinition,
 ): string => {
-  if (value === null) return '-';
-
+  const isNull = value === null;
   const isArray = Array.isArray(value);
 
   switch (getFieldKind(field.fieldType)) {
     case FieldKind.Checkbox:
-      return value === 'true' ? 'Yes' : 'No';
+      return isArray || !value || value === 'false' ? 'No' : 'Yes';
     case FieldKind.Date:
     case FieldKind.Month:
     case FieldKind.Week:
-      return !isArray ? value : '-';
+      return isArray || isNull ? '-' : value;
     case FieldKind.Reference:
       // TODO: Lookup reference form?
-      return !isArray ? value : '-';
+      return isArray || isNull ? '-' : value;
     case FieldKind.SingleSelect:
       return !isArray
         ? field.fieldType.singleSelect?.options.find((o) => o.id === value)
@@ -32,10 +33,14 @@ export const formatFieldValue = (
                   ?.name ?? '-',
             )
             .join(', ')
-        : '-';
+        : field.fieldType.multiSelect?.options.find((o) => o.id === value)
+            ?.name ?? '-';
     case FieldKind.SubForm:
       return 'Error: Subform fields should not reach this point';
-    default:
-      return isArray ? value.join(', ') : value.toString();
+    default: {
+      if (isNull) return '-';
+      if (isArray) return value.join(', ');
+      return value.toString();
+    }
   }
 };
