2909f70be1004dc7a2b5b4e51c939d113fe38558
Ben McAlindin <bmcalindin@gmail.com>
2022-03-09 16:14:43 +0100

    Add text field

--- frontend/apps/core-app/src/components/RecordEditor/FieldInput/TextFieldInput.component.tsx
+++ frontend/apps/core-app/src/components/RecordEditor/FieldInput/TextFieldInput.component.tsx
@@ -1,7 +1,32 @@
 import * as React from 'react';
+import { FormControl, Input } from 'native-base';
+import { useFormContext, useController } from 'react-hook-form';
+import { FormDefinition } from 'core-api-client';
 
-type Props = {};
+type Props = {
+  formId: string;
+  field: FormDefinition['fields'][number];
+};
 
-export const TextFieldInput: React.FC<Props> = () => {
-  return <></>;
+export const TextFieldInput: React.FC<Props> = ({ formId, field }) => {
+  const { control } = useFormContext();
+
+  const {
+    field: { onChange, onBlur, value, ref },
+    fieldState: { error, invalid },
+  } = useController({
+    name: `${formId}.${field.id}`,
+    control,
+    rules: {}, // TODO Record validation
+    defaultValue: '',
+  });
+
+  return (
+    <FormControl isInvalid={invalid}>
+      <FormControl.Label>Name</FormControl.Label>
+      <Input ref={ref} onBlur={onBlur} onChangeText={onChange} value={value} />
+      <FormControl.HelperText>{field.description}</FormControl.HelperText>
+      <FormControl.ErrorMessage>{error}</FormControl.ErrorMessage>
+    </FormControl>
+  );
 };
