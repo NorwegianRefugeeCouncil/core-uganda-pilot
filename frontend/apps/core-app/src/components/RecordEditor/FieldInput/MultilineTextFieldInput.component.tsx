60260c198039b4446cd6e5a2688b8a83ad6279a5
Ben McAlindin <bmcalindin@gmail.com>
2022-03-09 16:14:49 +0100

    Add multiline text field

--- frontend/apps/core-app/src/components/RecordEditor/FieldInput/MultilineTextFieldInput.component.tsx
+++ frontend/apps/core-app/src/components/RecordEditor/FieldInput/MultilineTextFieldInput.component.tsx
@@ -1,7 +1,37 @@
 import * as React from 'react';
+import { FormControl, TextArea } from 'native-base';
+import { useFormContext, useController } from 'react-hook-form';
+import { FormDefinition } from 'core-api-client';
 
-type Props = {};
+type Props = {
+  formId: string;
+  field: FormDefinition['fields'][number];
+};
 
-export const MultilineTextFieldInput: React.FC<Props> = () => {
-  return <></>;
+export const MultilineTextFieldInput: React.FC<Props> = ({ formId, field }) => {
+  const { control } = useFormContext();
+
+  const {
+    field: { onChange, onBlur, value, ref },
+    fieldState: { error, invalid },
+  } = useController({
+    name: `${formId}.${field.id}`,
+    control,
+    rules: {}, // TODO Record validation
+    defaultValue: '',
+  });
+
+  return (
+    <FormControl isInvalid={invalid}>
+      <FormControl.Label size="xs">{field.name}</FormControl.Label>
+      <TextArea
+        ref={ref}
+        onBlur={onBlur}
+        onChangeText={onChange}
+        value={value}
+      />
+      <FormControl.HelperText>{field.description}</FormControl.HelperText>
+      <FormControl.ErrorMessage>{error}</FormControl.ErrorMessage>
+    </FormControl>
+  );
 };
