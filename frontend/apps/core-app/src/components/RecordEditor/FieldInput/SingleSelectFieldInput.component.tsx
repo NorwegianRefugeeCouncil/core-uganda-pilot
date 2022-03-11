153d233bd4a8e6b41004c843c419cf5ebd389fc9
Ben McAlindin <bmcalindin@gmail.com>
2022-03-11 21:00:21 +0100

    Implement SingleSelectFieldInput

--- frontend/apps/core-app/src/components/RecordEditor/FieldInput/SingleSelectFieldInput.component.tsx
+++ frontend/apps/core-app/src/components/RecordEditor/FieldInput/SingleSelectFieldInput.component.tsx
@@ -1,7 +1,35 @@
 import * as React from 'react';
+import { FormControl, Select } from 'native-base';
+import { useFormContext, useController } from 'react-hook-form';
+import { FormDefinition } from 'core-api-client';
 
-type Props = {};
+type Props = {
+  formId: string;
+  field: FormDefinition['fields'][number];
+};
 
-export const SingleSelectFieldInput: React.FC<Props> = () => {
-  return <></>;
+export const SingleSelectFieldInput: React.FC<Props> = ({ formId, field }) => {
+  const { control } = useFormContext();
+
+  const {
+    field: { onChange, value },
+    fieldState: { error, invalid },
+  } = useController({
+    name: `${formId}.${field.id}`,
+    control,
+    rules: {}, // TODO Record validation
+  });
+
+  return (
+    <FormControl isRequired={field.required} isInvalid={invalid}>
+      <FormControl.Label>{field.name}</FormControl.Label>
+      <Select mt="1" onValueChange={onChange} selectedValue={value}>
+        {field.fieldType.singleSelect?.options.map((option) => (
+          <Select.Item key={option.id} label={option.name} value={option.id} />
+        ))}
+      </Select>
+      <FormControl.HelperText>{field.description}</FormControl.HelperText>
+      <FormControl.ErrorMessage>{error}</FormControl.ErrorMessage>
+    </FormControl>
+  );
 };
