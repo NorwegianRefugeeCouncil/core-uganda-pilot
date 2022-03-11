ad08d6fec244b4e1edc45f8e04641a18f1d7a425
Ben McAlindin <bmcalindin@gmail.com>
2022-03-11 23:20:06 +0100

    Implement ReferenceFieldInput

--- frontend/apps/core-app/src/components/RecordEditor/FieldInput/ReferenceFieldInput.component.tsx
+++ frontend/apps/core-app/src/components/RecordEditor/FieldInput/ReferenceFieldInput.component.tsx
@@ -1,7 +1,59 @@
 import * as React from 'react';
+import { FormControl, Select } from 'native-base';
+import { useFormContext, useController } from 'react-hook-form';
+import { FormDefinition, Record } from 'core-api-client';
 
-type Props = {};
+import { formsClient } from '../../../clients/formsClient';
 
-export const ReferenceFieldInput: React.FC<Props> = () => {
-  return <></>;
+type Props = {
+  formId: string;
+  databaseId: string;
+  field: FormDefinition['fields'][number];
+};
+
+export const ReferenceFieldInput: React.FC<Props> = ({
+  formId,
+  databaseId,
+  field,
+}) => {
+  const [referenceRecords, setReferenceRecords] = React.useState<Record[]>([]);
+
+  React.useEffect(() => {
+    (async () => {
+      if (field.fieldType.reference) {
+        const r = await formsClient.Record.list({
+          formId: field.fieldType.reference.formId,
+          databaseId: field.fieldType.reference.databaseId,
+        });
+        if (r.response) setReferenceRecords(r.response.items);
+        else setReferenceRecords([]);
+      } else {
+        setReferenceRecords([]);
+      }
+    })();
+  }, [field.fieldType.reference?.formId, databaseId]);
+
+  const { control } = useFormContext();
+
+  const {
+    field: { onChange, value },
+    fieldState: { error, invalid },
+  } = useController({
+    name: `${formId}.${field.id}`,
+    control,
+    rules: {}, // TODO Record validation
+  });
+
+  return (
+    <FormControl isRequired={field.required} isInvalid={invalid}>
+      <FormControl.Label>{field.name}</FormControl.Label>
+      <Select mt="1" onValueChange={onChange} selectedValue={value}>
+        {referenceRecords.map((r) => (
+          <Select.Item key={r.id} label={r.id} value={r.id} />
+        ))}
+      </Select>
+      <FormControl.HelperText>{field.description}</FormControl.HelperText>
+      <FormControl.ErrorMessage>{error}</FormControl.ErrorMessage>
+    </FormControl>
+  );
 };
