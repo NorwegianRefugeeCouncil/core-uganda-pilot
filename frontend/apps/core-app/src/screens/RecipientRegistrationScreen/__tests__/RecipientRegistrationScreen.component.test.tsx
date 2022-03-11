f945ca56a402488a75759d702b515410bf5cbd7a
Ben McAlindin <bmcalindin@gmail.com>
2022-03-11 10:28:23 +0100

    Fix props in test

--- frontend/apps/core-app/src/screens/RecipientRegistrationScreen/__tests__/RecipientRegistrationScreen.component.test.tsx
+++ frontend/apps/core-app/src/screens/RecipientRegistrationScreen/__tests__/RecipientRegistrationScreen.component.test.tsx
@@ -1,10 +1,59 @@
+import { FormDefinition, FormType, Record } from 'core-api-client';
+
 import { render } from '../../../testUtils/render';
+import { buildDefaultRecord } from '../buildDefaultRecord';
 import { RecipientRegistrationScreenComponent } from '../RecipientRegistrationScreen.component';
 
+jest.mock('../../../components/RecordEditor', () => {
+  const { View, Text } = jest.requireActual('react-native');
+  return {
+    RecordEditor: ({
+      form,
+      record,
+      onChange,
+    }: {
+      form: FormDefinition;
+      record: Record;
+      onChange: () => void;
+    }) => (
+      <View>
+        <Text>{JSON.stringify(form)}</Text>
+        <Text>{JSON.stringify(record)}</Text>
+      </View>
+    ),
+  };
+});
+
 it('should match the snapshot', () => {
+  const makeForm = (i: number): FormDefinition => ({
+    id: `form-id-${i}`,
+    code: 'form-code',
+    databaseId: 'database-id',
+    folderId: 'folder-id',
+    name: `form-name-${i}`,
+    formType: FormType.DefaultFormType,
+    fields: [
+      {
+        id: `field-id-${i}`,
+        name: `field-name-${i}`,
+        code: '',
+        description: '',
+        required: false,
+        key: false,
+        fieldType: { text: {} },
+      },
+    ],
+  });
+
   const { toJSON } = render(
     <RecipientRegistrationScreenComponent
-      route={{ name: 'RecipientRegistration', params: {}, key: 'key' }}
+      forms={[makeForm(1), makeForm(2)]}
+      records={[
+        buildDefaultRecord(makeForm(1)),
+        buildDefaultRecord(makeForm(2)),
+      ]}
+      onSubmit={() => {}}
+      onCancel={() => {}}
     />,
   );
   expect(toJSON()).toMatchSnapshot();
