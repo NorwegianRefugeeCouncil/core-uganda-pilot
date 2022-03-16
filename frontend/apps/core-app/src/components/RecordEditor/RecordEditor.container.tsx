1213b64c6aa8f0e9d107c26b3138c9e54d3dfd89
Ben McAlindin <bmcalindin@gmail.com>
2022-03-16 14:29:50 +0100

    Ensure record editor is being used within a form context

--- frontend/apps/core-app/src/components/RecordEditor/RecordEditor.container.tsx
+++ frontend/apps/core-app/src/components/RecordEditor/RecordEditor.container.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { FormDefinition } from 'core-api-client';
+import { useFormContext } from 'react-hook-form';
 
 import { RecordEditorComponent } from './RecordEditor.component';
 
@@ -8,5 +9,9 @@ type Props = {
 };
 
 export const RecordEditorContainer: React.FC<Props> = ({ form }) => {
+  const formContext = useFormContext();
+
+  if (!formContext) throw new Error('Form context is not available');
+
   return <RecordEditorComponent form={form} />;
 };
