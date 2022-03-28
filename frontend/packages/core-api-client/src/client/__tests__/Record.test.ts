3181efb737daba64b9d21d2da8deebe0a0c682e8
Ben McAlindin <bmcalindin@gmail.com>
2022-03-28 21:04:29 +0200

    Add createWithSubRecords tests

--- frontend/packages/core-api-client/src/client/__tests__/Record.test.ts
+++ frontend/packages/core-api-client/src/client/__tests__/Record.test.ts
@@ -86,3 +86,408 @@ describe('buildDefaultRecord', () => {
   });
 });
 
+describe('createWithSubRecords', () => {
+  const makeSubFormFields = (count: number): FieldDefinition[] =>
+    new Array(count).fill(0).map((_, i) => ({
+      id: `field-id-${i}`,
+      name: `field-name-${i}`,
+      description: '',
+      code: '',
+      required: false,
+      key: false,
+      fieldType: {
+        subForm: {
+          fields: [
+            {
+              id: `sub-field-id-${i}`,
+              name: `sub-field-name-${i}`,
+              description: '',
+              code: '',
+              required: false,
+              key: false,
+              fieldType: { text: {} },
+            },
+          ],
+        },
+      },
+    }));
+
+  const makeSubFormValue = (i: number, count: number): FieldValue => ({
+    fieldId: `field-id-${i}`,
+    value: new Array(count)
+      .fill(0)
+      .map((_, j) => [
+        { fieldId: `sub-field-id-${j}`, value: `sub-field-value-${j}` },
+      ]),
+  });
+
+  const makeFormWithRecord = (
+    fieldCount: number,
+    valueCount: number,
+  ): FormWithRecord<Record> => {
+    const form: FormDefinition = {
+      id: 'form-id',
+      name: 'form-name',
+      databaseId: 'database-id',
+      code: '',
+      folderId: '',
+      formType: FormType.DefaultFormType,
+      fields: makeSubFormFields(fieldCount),
+    };
+
+    const record = {
+      ...client.Record.buildDefaultRecord(form),
+      values: new Array(fieldCount)
+        .fill(0)
+        .map((_, i) => makeSubFormValue(i, valueCount)),
+    };
+
+    return {
+      form,
+      record,
+    };
+  };
+
+  const addRecordId = (record: Record, i: number): Record => ({
+    ...record,
+    id: `record-id-${i}`,
+  });
+
+  describe('success', () => {
+    it('should create a record without a sub record', async () => {
+      const inputData = makeFormWithRecord(0, 0);
+      const createdRecord = addRecordId(inputData.record, 0);
+
+      const createRecordSpy = jest
+        .spyOn(client.Record, 'create')
+        .mockImplementationOnce((request) =>
+          Promise.resolve({
+            success: true,
+            error: undefined,
+            response: createdRecord,
+            request,
+            status: 'ok',
+            statusCode: 200,
+          }),
+        );
+
+      const getRecordSpy = jest
+        .spyOn(client.Record, 'get')
+        .mockImplementationOnce((request) =>
+          Promise.resolve({
+            success: true,
+            error: undefined,
+            response: createdRecord,
+            request,
+            status: 'ok',
+            statusCode: 200,
+          }),
+        );
+
+      const result = await client.Record.createWithSubRecords(inputData);
+
+      expect(createRecordSpy).toHaveBeenCalledTimes(1);
+      expect(createRecordSpy).toHaveBeenCalledWith({
+        object: inputData.record,
+      });
+
+      expect(getRecordSpy).toHaveBeenCalledTimes(1);
+      expect(getRecordSpy).toHaveBeenCalledWith({
+        recordId: createdRecord.id,
+        databaseId: createdRecord.databaseId,
+        formId: createdRecord.formId,
+      });
+
+      expect(result).toEqual(createdRecord);
+    });
+
+    it('should create a record with a sub record', async () => {
+      const inputData = makeFormWithRecord(1, 1);
+
+      const createdRecord: Record = addRecordId(inputData.record, 0);
+
+      const createdSubrecord: Record = {
+        id: 'created-sub-record-id-0',
+        databaseId: inputData.form.databaseId,
+        formId: inputData.form.fields[0].id,
+        ownerId: createdRecord.id,
+        values: (
+          inputData.record.values[0] as {
+            fieldId: string;
+            value: FieldValue[][];
+          }
+        ).value[0],
+      };
+
+      const createRecordSpy = jest
+        .spyOn(client.Record, 'create')
+        .mockImplementationOnce((request) =>
+          Promise.resolve({
+            success: true,
+            error: undefined,
+            response: { ...createdRecord, values: [] },
+            request,
+            status: 'ok',
+            statusCode: 200,
+          }),
+        )
+        .mockImplementationOnce((request) =>
+          Promise.resolve({
+            success: true,
+            error: undefined,
+            response: createdSubrecord,
+            request,
+            status: 'ok',
+            statusCode: 200,
+          }),
+        );
+
+      const getRecordSpy = jest
+        .spyOn(client.Record, 'get')
+        .mockImplementationOnce((request) =>
+          Promise.resolve({
+            success: true,
+            error: undefined,
+            response: createdRecord,
+            request,
+            status: 'ok',
+            statusCode: 200,
+          }),
+        );
+
+      const result = await client.Record.createWithSubRecords(inputData);
+
+      expect(createRecordSpy).toHaveBeenCalledTimes(2);
+      expect(createRecordSpy).toHaveBeenCalledWith({
+        object: { ...inputData.record, values: [] },
+      });
+      expect(createRecordSpy).toHaveBeenCalledWith({
+        object: {
+          ...createdSubrecord,
+          id: '',
+        },
+      });
+
+      expect(getRecordSpy).toHaveBeenCalledTimes(1);
+      expect(getRecordSpy).toHaveBeenCalledWith({
+        recordId: createdRecord.id,
+        databaseId: createdRecord.databaseId,
+        formId: createdRecord.formId,
+      });
+
+      expect(result).toEqual(createdRecord);
+    });
+
+    it('should create a record with multiple sub records of the same field', async () => {
+      const inputData = makeFormWithRecord(1, 2);
+
+      const createdRecord: Record = addRecordId(inputData.record, 0);
+
+      const createdSubrecordA: Record = {
+        id: 'created-sub-record-id-0',
+        databaseId: inputData.form.databaseId,
+        formId: inputData.form.fields[0].id,
+        ownerId: createdRecord.id,
+        values: (
+          inputData.record.values[0] as {
+            fieldId: string;
+            value: FieldValue[][];
+          }
+        ).value[0],
+      };
+
+      const createdSubrecordB: Record = {
+        id: 'created-sub-record-id-0',
+        databaseId: inputData.form.databaseId,
+        formId: inputData.form.fields[0].id,
+        ownerId: createdRecord.id,
+        values: (
+          inputData.record.values[0] as {
+            fieldId: string;
+            value: FieldValue[][];
+          }
+        ).value[1],
+      };
+
+      const createRecordSpy = jest
+        .spyOn(client.Record, 'create')
+        .mockImplementationOnce((request) =>
+          Promise.resolve({
+            success: true,
+            error: undefined,
+            response: { ...createdRecord, values: [] },
+            request,
+            status: 'ok',
+            statusCode: 200,
+          }),
+        )
+        .mockImplementationOnce((request) =>
+          Promise.resolve({
+            success: true,
+            error: undefined,
+            response: createdSubrecordA,
+            request,
+            status: 'ok',
+            statusCode: 200,
+          }),
+        )
+        .mockImplementationOnce((request) =>
+          Promise.resolve({
+            success: true,
+            error: undefined,
+            response: createdSubrecordB,
+            request,
+            status: 'ok',
+            statusCode: 200,
+          }),
+        );
+
+      const getRecordSpy = jest
+        .spyOn(client.Record, 'get')
+        .mockImplementationOnce((request) =>
+          Promise.resolve({
+            success: true,
+            error: undefined,
+            response: createdRecord,
+            request,
+            status: 'ok',
+            statusCode: 200,
+          }),
+        );
+
+      const result = await client.Record.createWithSubRecords(inputData);
+
+      expect(createRecordSpy).toHaveBeenCalledTimes(3);
+      expect(createRecordSpy).toHaveBeenCalledWith({
+        object: { ...inputData.record, values: [] },
+      });
+      expect(createRecordSpy).toHaveBeenCalledWith({
+        object: {
+          ...createdSubrecordA,
+          id: '',
+        },
+      });
+      expect(createRecordSpy).toHaveBeenCalledWith({
+        object: {
+          ...createdSubrecordB,
+          id: '',
+        },
+      });
+
+      expect(getRecordSpy).toHaveBeenCalledTimes(1);
+      expect(getRecordSpy).toHaveBeenCalledWith({
+        recordId: createdRecord.id,
+        databaseId: createdRecord.databaseId,
+        formId: createdRecord.formId,
+      });
+
+      expect(result).toEqual(createdRecord);
+    });
+  });
+
+  it('should create a record with multiple sub records of different field', async () => {
+    const inputData = makeFormWithRecord(2, 1);
+
+    const createdRecord: Record = addRecordId(inputData.record, 0);
+
+    const createdSubrecordA: Record = {
+      id: 'created-sub-record-id-0',
+      databaseId: inputData.form.databaseId,
+      formId: inputData.form.fields[0].id,
+      ownerId: createdRecord.id,
+      values: (
+        inputData.record.values[0] as {
+          fieldId: string;
+          value: FieldValue[][];
+        }
+      ).value[0],
+    };
+
+    const createdSubrecordB: Record = {
+      id: 'created-sub-record-id-1',
+      databaseId: inputData.form.databaseId,
+      formId: inputData.form.fields[1].id,
+      ownerId: createdRecord.id,
+      values: (
+        inputData.record.values[1] as {
+          fieldId: string;
+          value: FieldValue[][];
+        }
+      ).value[0],
+    };
+
+    const createRecordSpy = jest
+      .spyOn(client.Record, 'create')
+      .mockImplementationOnce((request) =>
+        Promise.resolve({
+          success: true,
+          error: undefined,
+          response: { ...createdRecord, values: [] },
+          request,
+          status: 'ok',
+          statusCode: 200,
+        }),
+      )
+      .mockImplementationOnce((request) =>
+        Promise.resolve({
+          success: true,
+          error: undefined,
+          response: createdSubrecordA,
+          request,
+          status: 'ok',
+          statusCode: 200,
+        }),
+      )
+      .mockImplementationOnce((request) =>
+        Promise.resolve({
+          success: true,
+          error: undefined,
+          response: createdSubrecordB,
+          request,
+          status: 'ok',
+          statusCode: 200,
+        }),
+      );
+
+    const getRecordSpy = jest
+      .spyOn(client.Record, 'get')
+      .mockImplementationOnce((request) =>
+        Promise.resolve({
+          success: true,
+          error: undefined,
+          response: createdRecord,
+          request,
+          status: 'ok',
+          statusCode: 200,
+        }),
+      );
+
+    const result = await client.Record.createWithSubRecords(inputData);
+
+    expect(createRecordSpy).toHaveBeenCalledTimes(3);
+    expect(createRecordSpy).toHaveBeenCalledWith({
+      object: { ...inputData.record, values: [] },
+    });
+    expect(createRecordSpy).toHaveBeenCalledWith({
+      object: {
+        ...createdSubrecordA,
+        id: '',
+      },
+    });
+    expect(createRecordSpy).toHaveBeenCalledWith({
+      object: {
+        ...createdSubrecordB,
+        id: '',
+      },
+    });
+
+    expect(getRecordSpy).toHaveBeenCalledTimes(1);
+    expect(getRecordSpy).toHaveBeenCalledWith({
+      recordId: createdRecord.id,
+      databaseId: createdRecord.databaseId,
+      formId: createdRecord.formId,
+    });
+
+    expect(result).toEqual(createdRecord);
+  });
+});
