e4bac8028b087430022eb630c971bf0666d75231
Ben McAlindin <bmcalindin@gmail.com>
2022-04-21 10:05:09 +0200

    Use else statement

--- frontend/packages/core-api-client/src/validation/Recipient.ts
+++ frontend/packages/core-api-client/src/validation/Recipient.ts
@@ -10,21 +10,21 @@ export const validateRecipientHierarch = (
     if (idx === 0) {
       // assert no key field
       if (form.fields.find((f) => f.key)) throw new Error();
+    } else {
+      // assert reference key field
+      const keyField = form.fields.find(
+        (f) => f.key && getFieldKind(f.fieldType) === FieldKind.Reference,
+      );
+
+      if (!keyField) throw new Error();
+
+      const { form: prevForm } = arr[idx - 1];
+
+      if (
+        keyField.fieldType.reference?.formId !== prevForm.id ||
+        keyField.fieldType.reference?.databaseId !== prevForm.databaseId
+      )
+        throw new Error();
     }
-
-    // assert reference key field
-    const keyField = form.fields.find(
-      (f) => f.key && getFieldKind(f.fieldType) === FieldKind.Reference,
-    );
-
-    if (!keyField) throw new Error();
-
-    const { form: prevForm } = arr[idx - 1];
-
-    if (
-      keyField.fieldType.reference?.formId !== prevForm.id ||
-      keyField.fieldType.reference?.databaseId !== prevForm.databaseId
-    )
-      throw new Error();
   });
 };
