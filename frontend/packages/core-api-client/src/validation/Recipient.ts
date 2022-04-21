362cfed7eff27caa5fb7ed50236a4a44a0b3a828
Ben McAlindin <bmcalindin@gmail.com>
2022-04-21 12:59:29 +0200

    Add error messages

--- frontend/packages/core-api-client/src/validation/Recipient.ts
+++ frontend/packages/core-api-client/src/validation/Recipient.ts
@@ -5,18 +5,22 @@ export const validateRecipientHierarch = (
   formWithRecordList: FormWithRecord<Record>[],
 ): void => {
   formWithRecordList.forEach(({ form, record }, idx, arr) => {
-    if (form.id !== record.formId) throw new Error();
+    if (form.id !== record.formId)
+      throw new Error('Record is not associated with the form');
 
     if (idx === 0) {
       // assert no key field
-      if (form.fields.find((f) => f.key)) throw new Error();
+      if (form.fields.find((f) => f.key))
+        throw new Error(
+          `Root recipient form ${form.id} should not have a key field`,
+        );
     } else {
       // assert reference key field
       const keyField = form.fields.find(
         (f) => f.key && getFieldKind(f.fieldType) === FieldKind.Reference,
       );
 
-      if (!keyField) throw new Error();
+      if (!keyField) throw new Error(`No key field found for form ${form.id}`);
 
       const { form: prevForm } = arr[idx - 1];
 
