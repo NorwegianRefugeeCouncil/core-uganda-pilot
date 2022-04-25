96661ac2ed49516111c28ec8db7fe344991eb5d9
internetti <irena.laemmerer@gmail.com>
2022-04-25 12:29:29 +0200

    COR-491: remove replaced files

--- frontend/apps/core-app/src/components/Table/createTableColumns.ts
+++
@@ -1,8 +0,0 @@
-import { FormDefinition } from 'core-api-client';
-import { Column } from 'react-table';
-
-export const createTableColumns = (form: FormDefinition): Column[] =>
-  form.fields.map((field) => ({
-    Header: field.name,
-    accessor: field.id,
-  }));
--- frontend/apps/core-app/src/components/Table/index.ts
+++
@@ -1,4 +0,0 @@
-import { TableContainer } from './Table.container';
-
-export default TableContainer;
-export { GlobalTableFilter } from './GlobalTableFilter';
