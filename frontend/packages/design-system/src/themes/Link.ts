561010f1aa5987ccdcc463482f343b2075871d42
Ben McAlindin <bmcalindin@gmail.com>
2022-04-25 13:39:25 +0200

    Link theme

---
+++ frontend/packages/design-system/src/themes/Link.ts
@@ -0,0 +1,17 @@
+export default {
+  defaultProps: {
+    isUnderlined: false,
+  },
+  baseStyle: {
+    _text: {
+      fontSize: 'xs',
+      fontWeight: 'medium',
+      color: 'secondary.500',
+    },
+    _hover: {
+      _text: {
+        color: 'primary.500',
+      },
+    },
+  },
+};
