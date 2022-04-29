04db1766c0d57f14a60af5e7485c91ffcac16ce6
Ben McAlindin <bmcalindin@gmail.com>
2022-04-29 10:52:06 +0200

    Add util functions for handling trees

---
+++ frontend/packages/core-api-client/src/utils/tree.ts
@@ -0,0 +1,35 @@
+type Tree<T> = T & { childNodes: Tree<T>[] };
+
+export const createDataTree = <T>(
+  dataset: T[],
+  idProperty: keyof T,
+  parentIdProperty: keyof T,
+): Tree<T>[] => {
+  const hashTable = Object.create(null);
+  dataset.forEach((aData) => {
+    hashTable[aData[idProperty]] = { ...aData, childNodes: [] };
+  });
+  const dataTree: Tree<T>[] = [];
+  dataset.forEach((aData) => {
+    if (aData[parentIdProperty])
+      hashTable[aData[parentIdProperty]].childNodes.push(
+        hashTable[aData[idProperty]],
+      );
+    else dataTree.push(hashTable[aData[idProperty]]);
+  });
+  return dataTree;
+};
+
+export const getLeafNodes = <T>(
+  nodes: Tree<T>[],
+  result: Tree<T>[] = [],
+): Tree<T>[] => {
+  for (let i = 0, { length } = nodes; i < length; i++) {
+    if (!nodes[i].childNodes || nodes[i].childNodes.length === 0) {
+      result.push(nodes[i]);
+    } else {
+      return getLeafNodes(nodes[i].childNodes, result);
+    }
+  }
+  return result;
+};
