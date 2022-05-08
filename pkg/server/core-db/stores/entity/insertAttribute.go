92338a1e4bf5f5e250c94b1aa1866bba9c871d72
Ben McAlindin <bmcalindin@gmail.com>
2022-05-08 21:43:27 +0200

    Implement InsertAttribute

--- pkg/server/core-db/stores/entity/insertAttribute.go
+++ pkg/server/core-db/stores/entity/insertAttribute.go
@@ -8,5 +8,44 @@ import (
 )
 
 func (d *entityPostgresStore) InsertAttribute(ctx context.Context, db *gorm.DB, attribute types.Attribute) (*types.Attribute, error) {
-	return nil, nil
+	ddl := d.sqlBuilder.InsertRow(
+		"",
+		"entity_attribute",
+		[]string{
+			"id",
+			"name",
+			"list",
+			"type",
+			"entity_id",
+			"constraint_required",
+			"constraint_unique",
+			"constraint_min",
+			"constraint_max",
+			"constraint_pattern",
+			"constraint_enum",
+			"constraint_custom",
+		},
+		[]any{
+			attribute.ID,
+			attribute.Name,
+			attribute.List,
+			attribute.Type,
+			attribute.EntityID,
+			attribute.Constraints.Required,
+			attribute.Constraints.Unique,
+			attribute.Constraints.Min,
+			attribute.Constraints.Max,
+			attribute.Constraints.Pattern,
+			attribute.Constraints.Enum,
+			attribute.Constraints.Custom,
+		},
+	)
+
+	result := db.Exec(ddl.Query, ddl.Args...)
+
+	if err := result.Error; err != nil {
+		return nil, err
+	}
+
+	return &attribute, nil
 }
