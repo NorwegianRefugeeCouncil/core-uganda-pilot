9396ead70bbb2a48b67041ea5cacb49385f0ca82
Ben McAlindin <bmcalindin@gmail.com>
2022-05-08 21:44:02 +0200

    Create EntityService.Create

--- pkg/server/core-db/services/entity/create.go
+++ pkg/server/core-db/services/entity/create.go
@@ -4,8 +4,58 @@ import (
 	"context"
 
 	"github.com/nrc-no/core/pkg/server/core-db/types"
+	uuid "github.com/satori/go.uuid"
 )
 
 func (d *entityService) Create(ctx context.Context, entity types.Entity) (*types.Entity, error) {
-	return nil, nil
+	tx := d.transactionStore.Begin()
+
+	entityId := uuid.NewV4()
+
+	entityDefinition, err := d.entityStore.InsertEntityDefinition(ctx, tx, types.EntityDefinition{
+		ID:          entityId.String(),
+		Name:        entity.Name,
+		Description: entity.Description,
+		Constraints: entity.Constraints,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	attributes := make([]types.Attribute, len(entity.Attributes))
+	for i, attribute := range entity.Attributes {
+		attributeId := uuid.NewV4()
+		attribute.ID = attributeId.String()
+
+		a, err := d.entityStore.InsertAttribute(ctx, tx, attribute)
+
+		if err != nil {
+			return nil, err
+		}
+
+		attributes[i] = *a
+	}
+
+	relationships := make([]types.EntityRelationship, len(entity.Relationships))
+	for i, relationship := range entity.Relationships {
+		relationshipId := uuid.NewV4()
+		relationship.ID = relationshipId.String()
+
+		r, err := d.entityStore.InsertRelationship(ctx, tx, relationship)
+
+		if err != nil {
+			return nil, err
+		}
+
+		relationships[i] = *r
+	}
+
+	createdEntity := types.Entity{
+		EntityDefinition: *entityDefinition,
+		Attributes:       attributes,
+		Relationships:    relationships,
+	}
+
+	return &createdEntity, nil
 }
