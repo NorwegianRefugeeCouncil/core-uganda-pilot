7b6903ac88cb9dc3d5358373037eb80580d5c29e
Ben McAlindin <bmcalindin@gmail.com>
2022-05-09 12:51:17 +0200

    Use pointers to allow for null values

--- pkg/server/core-db/types/AttributeContstraints.go
+++ pkg/server/core-db/types/AttributeContstraints.go
@@ -5,9 +5,9 @@ type AttributeConstraintType string
 type AttributeConstraints struct {
 	Required bool     `json:"required"`
 	Unique   bool     `json:"unique"`
-	Min      float32  `json:"min"`
-	Max      float32  `json:"max"`
-	Pattern  string   `json:"pattern"`
+	Min      *float32 `json:"min"`
+	Max      *float32 `json:"max"`
+	Pattern  *string  `json:"pattern"`
 	Enum     []string `json:"enum"`
 	Custom   []string `json:"custom"`
 }
